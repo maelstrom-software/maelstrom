d6759883c15dfa9544353b7aa24bcf5ce563941c
Neal Fachan <neal@fachan.com>
2023-05-26 13:46:18 -0700

    Change broker main loop to look like worker's.

--- src/broker.rs
+++ src/broker.rs
@@ -1,11 +1,9 @@
 mod heap;
 pub mod scheduler; // XXX make private
 
-use crate::{proto, Result};
+use crate::{proto, Error, Result};
 
-/// The main function for the client. This should be called on a task of its own. It will return
-/// when a signal is received or when all work has been processed by the broker.
-pub async fn main(port: Option<u16>) -> Result<()> {
+async fn listener_main(port: Option<u16>) -> Result<()> {
     let sockaddr =
         std::net::SocketAddrV6::new(std::net::Ipv6Addr::UNSPECIFIED, port.unwrap_or(0), 0, 0);
     let listener = tokio::net::TcpListener::bind(sockaddr).await?;
@@ -16,5 +14,26 @@ pub async fn main(port: Option<u16>) -> Result<()> {
         let hello: proto::Hello = proto::read_message(&mut socket).await?;
         println!("got: {hello:?}");
         std::mem::forget(socket);
+    }
+}
+
+async fn signal_handler(kind: tokio::signal::unix::SignalKind) -> Result<()> {
+    tokio::signal::unix::signal(kind)?.recv().await;
+    Err(Error::msg(format!("received signal {:?}", kind)))
+}
+
+/// The main function for the client. This should be called on a task of its own. It will return
+/// when a signal is received or when all work has been processed by the broker.
+pub async fn main(port: Option<u16>) -> Result<()> {
+    let mut join_set = tokio::task::JoinSet::new();
+    join_set.spawn(async move { listener_main(port).await });
+    join_set.spawn(async { signal_handler(tokio::signal::unix::SignalKind::interrupt()).await });
+    join_set.spawn(async { signal_handler(tokio::signal::unix::SignalKind::terminate()).await });
+
+    loop {
+        join_set
+            .join_next()
+            .await
+            .expect("at least one task should return an error")??;
     }
 }
