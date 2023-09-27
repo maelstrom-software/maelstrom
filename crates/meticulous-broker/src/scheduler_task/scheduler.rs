95ba0c8e32d1181da04f991c4650eb6bc8cf4985
Neal Fachan <neal@fachan.com>
2023-09-27 17:13:29 +0000

    Move extension traits {Option,Bool}Ext into meticulous_util.

--- crates/meticulous-broker/src/scheduler_task/scheduler.rs
+++ crates/meticulous-broker/src/scheduler_task/scheduler.rs
@@ -7,7 +7,7 @@ use meticulous_base::{
     BrokerStatistics, ClientExecutionId, ClientId, ExecutionDetails, ExecutionId, ExecutionResult,
     Sha256Digest, WorkerId,
 };
-use meticulous_util::heap::{Heap, HeapDeps, HeapIndex};
+use meticulous_util::{heap::{Heap, HeapDeps, HeapIndex}, OptionExt, BoolExt};
 use std::{
     collections::{HashMap, HashSet, VecDeque},
     path::{Path, PathBuf},
@@ -119,34 +119,6 @@ impl<CacheT: SchedulerCache, DepsT: SchedulerDeps> Scheduler<CacheT, DepsT> {
  * |_|
  *  FIGLET: private
  */
-
-trait OptionExt {
-    fn assert_is_none(self);
-    fn assert_is_some(self);
-}
-
-impl<T> OptionExt for Option<T> {
-    fn assert_is_none(self) {
-        assert!(self.is_none());
-    }
-    fn assert_is_some(self) {
-        assert!(self.is_some());
-    }
-}
-
-trait BoolExt {
-    fn assert_is_true(self);
-    fn assert_is_false(self);
-}
-
-impl BoolExt for bool {
-    fn assert_is_true(self) {
-        assert!(self);
-    }
-    fn assert_is_false(self) {
-        assert!(!self);
-    }
-}
 
 struct Execution {
     details: ExecutionDetails,
--- crates/meticulous-util/src/lib.rs
+++ crates/meticulous-util/src/lib.rs
@@ -1,3 +1,37 @@
 pub mod error;
 pub mod heap;
 pub mod net;
+
+/// An extension trait that is useful for working with [std::collections::HashMap]s. A lot of times
+/// we just want to assert whether or not something previously existed in the
+/// [std::collections::HashMap]. This provides a convenient way to do that.
+pub trait OptionExt {
+    fn assert_is_none(self);
+    fn assert_is_some(self);
+}
+
+impl<T> OptionExt for Option<T> {
+    fn assert_is_none(self) {
+        assert!(self.is_none());
+    }
+    fn assert_is_some(self) {
+        assert!(self.is_some());
+    }
+}
+
+/// An extension trait that is useful for working with [std::collections::HashSet]s. A lot of times
+/// we just want to assert whether or not something previously existed in the
+/// [std::collections::HashSet]. This provides a convenient way to do that.
+pub trait BoolExt {
+    fn assert_is_true(self);
+    fn assert_is_false(self);
+}
+
+impl BoolExt for bool {
+    fn assert_is_true(self) {
+        assert!(self);
+    }
+    fn assert_is_false(self) {
+        assert!(!self);
+    }
+}
