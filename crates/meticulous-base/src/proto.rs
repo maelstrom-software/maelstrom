94e1553d09d23b82641e0d359aecb850532fae8b
Neal Fachan <neal@fachan.com>
2023-10-16 18:23:29 +0000

    Add more doc comments.

--- crates/meticulous-base/src/proto.rs
+++ crates/meticulous-base/src/proto.rs
@@ -1,10 +1,10 @@
-//! Messages sent between various binaries, and helper functions related to those messages.
+//! Messages sent between various binaries.
 
 use crate::{BrokerStatistics, ClientJobId, JobDetails, JobId, JobResult, Sha256Digest};
 use serde::{Deserialize, Serialize};
 
-/// The first message sent by a client or worker to the broker. It identifies the client/worker and
-/// gives any relevant information.
+/// The first message sent by a connector to the broker. It identifies what the connector is, and
+/// provides any relevant information.
 #[derive(Serialize, Deserialize, Debug)]
 pub enum Hello {
     Client,
@@ -14,7 +14,7 @@ pub enum Hello {
 }
 
 /// Message sent from the broker to a worker. The broker won't send a message until it has received
-/// a [Hello] and determined the type of its interlocutor.
+/// a [`Hello`] and determined the type of its interlocutor.
 #[derive(Clone, Debug, Deserialize, PartialEq, Serialize)]
 pub enum BrokerToWorker {
     EnqueueJob(JobId, JobDetails),
@@ -22,21 +22,13 @@ pub enum BrokerToWorker {
 }
 
 /// Message sent from a worker to the broker. These are responses to previous
-/// [BrokerToWorker::EnqueueJob] messages. After sending the initial [Hello], a worker will
-/// exclusively send a stream of these messages.
+/// [`BrokerToWorker::EnqueueJob`] messages. After sending the initial [`Hello`], a worker will
+/// send a stream of these messages.
 #[derive(Clone, Debug, Deserialize, PartialEq, Serialize)]
 pub struct WorkerToBroker(pub JobId, pub JobResult);
 
-/// Message sent from a client to the broker. After sending the initial [Hello], a client will
-/// exclusively send a stream of these messages.
-#[derive(Clone, Debug, Deserialize, PartialEq, Serialize)]
-pub enum ClientToBroker {
-    JobRequest(ClientJobId, JobDetails),
-    StatisticsRequest,
-}
-
 /// Message sent from the broker to a client. The broker won't send a message until it has recevied
-/// a [Hello] and determined the type of its interlocutor.
+/// a [`Hello`] and determined the type of its interlocutor.
 #[derive(Clone, Debug, Deserialize, PartialEq, Serialize)]
 pub enum BrokerToClient {
     JobResponse(ClientJobId, JobResult),
@@ -44,14 +36,37 @@ pub enum BrokerToClient {
     StatisticsResponse(BrokerStatistics),
 }
 
+/// Message sent from a client to the broker. After sending the initial [`Hello`], a client will
+/// send a stream of these messages.
 #[derive(Clone, Debug, Deserialize, PartialEq, Serialize)]
-pub struct ArtifactFetcherToBroker(pub Sha256Digest);
+pub enum ClientToBroker {
+    JobRequest(ClientJobId, JobDetails),
+    StatisticsRequest,
+}
 
+/// Message sent from the broker to an artifact fetcher. This will be in response to an
+/// [`ArtifactFetcherToBroker`] message. On success, the message contains the size of the artifact.
+/// The message is then be followed by exactly that many bytes of the artifact's body. On failure,
+/// the contains details about what went wrong. After a failure, the broker will close the artifact
+/// fetcher connection.
 #[derive(Clone, Debug, Deserialize, PartialEq, Serialize)]
 pub struct BrokerToArtifactFetcher(pub Result<u64, String>);
 
+/// Message sent from an artifact fetcher to the broker. It will be answered with a
+/// [`BrokerToArtifactFetcher`].
 #[derive(Clone, Debug, Deserialize, PartialEq, Serialize)]
-pub struct ArtifactPusherToBroker(pub Sha256Digest, pub u64);
+pub struct ArtifactFetcherToBroker(pub Sha256Digest);
 
+/// Message sent from the broker to an artifact pusher. This will be in response to an
+/// [`ArtifactPusherToBroker`] message and the artifact's body. On success, the message contains no
+/// other details, indicating that the artifact was successfully written to disk, and that the
+/// digest matched. On failure, this message contains details about what went wrong. After a
+/// failure, the broker will close the artifact pusher connection.
 #[derive(Clone, Debug, Deserialize, PartialEq, Serialize)]
 pub struct BrokerToArtifactPusher(pub Result<(), String>);
+
+/// Message sent from an artifact pusher to the broker. It contains the digest of the artifact
+/// that is about to be pushed as well as its size. The body of the artifact will immediately
+/// follow this message. It will be answered with a [`BrokerToArtifactPusher`].
+#[derive(Clone, Debug, Deserialize, PartialEq, Serialize)]
+pub struct ArtifactPusherToBroker(pub Sha256Digest, pub u64);
