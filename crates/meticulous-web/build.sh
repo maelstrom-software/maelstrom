730cb7dae904971fb2e7ea9ea4d0acbfbf894362
Remi Bernotavicius <remi@abort.cc>
2023-10-23 12:07:37 -0700

    Fix web_ui building that broke in the last patch
    The last patch had a bug where not all the files ended up in the tar

--- crates/meticulous-web/build.sh
+++ crates/meticulous-web/build.sh
@@ -6,7 +6,7 @@ pkg_dir=target/wasm_pkg/
 mkdir -p $pkg_dir
 
 pushd crates/meticulous-web
-wasm-pack build --release --target web --out-dir $pkg_dir
+wasm-pack build --release --target web --out-dir ../../$pkg_dir
 popd
 
 cp crates/meticulous-web/www/* $pkg_dir
