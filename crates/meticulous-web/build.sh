1eb44fd6cad258d8bff6a01f25d6a218d22dfcef
Remi Bernotavicius <remi@abort.cc>
2023-10-23 10:55:05 -0700

    Move the pkg directory to be in target/wasm_pkg

--- crates/meticulous-web/build.sh
+++ crates/meticulous-web/build.sh
@@ -2,9 +2,12 @@
 
 set -ex
 
+pkg_dir=target/wasm_pkg/
+mkdir -p $pkg_dir
+
 pushd crates/meticulous-web
-wasm-pack build --release --target web
+wasm-pack build --release --target web --out-dir $pkg_dir
 popd
 
-cp crates/meticulous-web/www/* crates/meticulous-web/pkg/
-tar --create --directory crates/meticulous-web/pkg . --file target/web.tar
+cp crates/meticulous-web/www/* $pkg_dir
+tar --create --directory $pkg_dir . --file target/web.tar
