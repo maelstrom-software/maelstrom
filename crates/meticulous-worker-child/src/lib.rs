05f4dcabfa61f1bead9b631569abf177fcf0d4fa
Neal Fachan <neal@fachan.com>
2023-11-06 18:30:50 +0000

    Create a new meticulous-worker-child crate.
    This will be a no_std crate used in the child process of the worker.
    
    It'll be no_std because after we clone we can't use the allocator or anything
    else where a thread in the parent may have held a lock at clone time.
    
    This initial version of the crate isn't no_std, but we'll work our way there.

---
+++ crates/meticulous-worker-child/src/lib.rs
@@ -0,0 +1,87 @@
+use anyhow::Result;
+use meticulous_base::JobDetails;
+use nix::{
+    errno::Errno,
+    unistd::{Gid, Uid},
+};
+use std::{
+    ffi::CString,
+    fs::{self, File},
+    io::Write as _,
+    iter,
+    os::fd::{FromRawFd as _, RawFd},
+    ptr,
+};
+
+/// The guts of the child code. This function can return a [`Result`].
+fn start_and_exec_in_child_inner(
+    details: &JobDetails,
+    stdout_write_fd: RawFd,
+    stderr_write_fd: RawFd,
+    _exec_result_write_fd: RawFd,
+    parent_uid: Uid,
+    parent_gid: Gid,
+) -> Result<()> {
+    fs::write("/proc/self/setgroups", "deny\n")?;
+    fs::write("/proc/self/uid_map", format!("0 {} 1\n", parent_uid))?;
+    fs::write("/proc/self/gid_map", format!("0 {} 1\n", parent_gid))?;
+    unsafe { nc::setsid() }.map_err(Errno::from_i32)?;
+    unsafe { nc::dup2(stdout_write_fd, 1) }.map_err(Errno::from_i32)?;
+    unsafe { nc::dup2(stderr_write_fd, 2) }.map_err(Errno::from_i32)?;
+    unsafe { nc::close_range(3, !0u32, nc::CLOSE_RANGE_CLOEXEC) }.map_err(Errno::from_i32)?;
+    let program_cstr = CString::new(details.program.as_str())?;
+    let program_ptr = program_cstr.as_bytes_with_nul().as_ptr();
+    let arguments = details
+        .arguments
+        .iter()
+        .map(String::as_str)
+        .map(CString::new)
+        .collect::<Result<Vec<_>, _>>()?;
+    let argument_ptrs = iter::once(program_ptr)
+        .chain(
+            arguments
+                .iter()
+                .map(|cstr| cstr.as_bytes_with_nul().as_ptr()),
+        )
+        .chain(iter::once(ptr::null()))
+        .collect::<Vec<_>>();
+    let env: [*const u8; 1] = [ptr::null()];
+    match unsafe {
+        nc::syscalls::syscall3(
+            nc::SYS_EXECVE,
+            program_ptr as usize,
+            argument_ptrs.as_ptr() as usize,
+            env.as_ptr() as usize,
+        )
+    } {
+        Err(errno) => Err(Errno::from_i32(errno).into()),
+        Ok(_) => unreachable!(),
+    }
+}
+
+/// Try to exec the job and write the error message to the pipe on failure.
+pub fn start_and_exec_in_child(
+    details: &JobDetails,
+    stdout_write_fd: RawFd,
+    stderr_write_fd: RawFd,
+    exec_result_write_fd: RawFd,
+    parent_uid: Uid,
+    parent_gid: Gid,
+) -> ! {
+    // TODO: https://github.com/meticulous-software/meticulous/issues/47
+    //
+    // We assume any error we encounter in the child is an execution error. While highly unlikely,
+    // we could theoretically encounter a system error.
+    let Err(err) = start_and_exec_in_child_inner(
+        details,
+        stdout_write_fd,
+        stderr_write_fd,
+        exec_result_write_fd,
+        parent_uid,
+        parent_gid,
+    ) else {
+        unreachable!();
+    };
+    let _ = unsafe { File::from_raw_fd(exec_result_write_fd) }.write_fmt(format_args!("{}", err));
+    std::process::exit(1);
+}
--- crates/meticulous-worker/src/executor.rs
+++ crates/meticulous-worker/src/executor.rs
@@ -10,14 +10,11 @@ use nix::{
     unistd::{self, Gid, Pid, Uid},
 };
 use std::{
-    ffi::CString,
-    fs::{self, File},
+    fs::File,
     io::Read as _,
-    io::Write as _,
-    iter, mem,
-    os::fd::{AsRawFd as _, FromRawFd as _, IntoRawFd as _, OwnedFd, RawFd},
+    mem,
+    os::fd::{AsRawFd as _, FromRawFd as _, IntoRawFd as _, OwnedFd},
     pin::Pin,
-    ptr,
     task::{Context, Poll},
 };
 use tokio::{
@@ -153,76 +150,6 @@ async fn output_reader_task_main(
 }
 
 impl Executor {
-    /// The guts of the child code. This function can return a [`Result`].
-    fn start_and_exec_in_child_inner(
-        &self,
-        details: &JobDetails,
-        stdout_write_fd: RawFd,
-        stderr_write_fd: RawFd,
-        _exec_result_write_fd: RawFd,
-    ) -> Result<()> {
-        fs::write("/proc/self/setgroups", "deny\n")?;
-        fs::write("/proc/self/uid_map", format!("0 {} 1\n", self.uid))?;
-        fs::write("/proc/self/gid_map", format!("0 {} 1\n", self.gid))?;
-        unsafe { nc::setsid() }.map_err(Errno::from_i32)?;
-        unsafe { nc::dup2(stdout_write_fd, 1) }.map_err(Errno::from_i32)?;
-        unsafe { nc::dup2(stderr_write_fd, 2) }.map_err(Errno::from_i32)?;
-        unsafe { nc::close_range(3, !0u32, nc::CLOSE_RANGE_CLOEXEC) }.map_err(Errno::from_i32)?;
-        let program_cstr = CString::new(details.program.as_str())?;
-        let program_ptr = program_cstr.as_bytes_with_nul().as_ptr();
-        let arguments = details
-            .arguments
-            .iter()
-            .map(String::as_str)
-            .map(CString::new)
-            .collect::<Result<Vec<_>, _>>()?;
-        let argument_ptrs = iter::once(program_ptr)
-            .chain(
-                arguments
-                    .iter()
-                    .map(|cstr| cstr.as_bytes_with_nul().as_ptr()),
-            )
-            .chain(iter::once(ptr::null()))
-            .collect::<Vec<_>>();
-        let env: [*const u8; 1] = [ptr::null()];
-        match unsafe {
-            nc::syscalls::syscall3(
-                nc::SYS_EXECVE,
-                program_ptr as usize,
-                argument_ptrs.as_ptr() as usize,
-                env.as_ptr() as usize,
-            )
-        } {
-            Err(errno) => Err(Errno::from_i32(errno).into()),
-            Ok(_) => unreachable!(),
-        }
-    }
-
-    /// Try to exec the job and write the error message to the pipe on failure.
-    fn start_and_exec_in_child(
-        &self,
-        details: &JobDetails,
-        stdout_write_fd: RawFd,
-        stderr_write_fd: RawFd,
-        exec_result_write_fd: RawFd,
-    ) -> ! {
-        // TODO: https://github.com/meticulous-software/meticulous/issues/47
-        //
-        // We assume any error we encounter in the child is an execution error. While highly unlikely,
-        // we could theoretically encounter a system error.
-        let Err(err) = self.start_and_exec_in_child_inner(
-            details,
-            stdout_write_fd,
-            stderr_write_fd,
-            exec_result_write_fd,
-        ) else {
-            unreachable!();
-        };
-        let _ =
-            unsafe { File::from_raw_fd(exec_result_write_fd) }.write_fmt(format_args!("{}", err));
-        std::process::exit(1);
-    }
-
     #[must_use]
     fn start_inner(
         &self,
@@ -282,11 +209,13 @@ impl Executor {
             mem::forget(stdout_read_fd);
             mem::forget(stderr_read_fd);
             mem::forget(exec_result_read_fd);
-            self.start_and_exec_in_child(
+            meticulous_worker_child::start_and_exec_in_child(
                 details,
                 stdout_write_fd.into_raw_fd(),
                 stderr_write_fd.into_raw_fd(),
                 exec_result_write_fd.into_raw_fd(),
+                self.uid,
+                self.gid,
             );
         }
 
