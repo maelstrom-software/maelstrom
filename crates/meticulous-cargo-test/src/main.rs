3739ce01660441d2d77575fba34f456d147b16cc
Remi Bernotavicius <remi@abort.cc>
2023-11-09 14:34:25 -0800

    cargo-metest: Refactor some things around queuing jobs

--- crates/meticulous-cargo-test/src/main.rs
+++ crates/meticulous-cargo-test/src/main.rs
@@ -1,5 +1,6 @@
 use anyhow::{anyhow, Result};
 use cargo::{get_cases_from_binary, CargoBuild};
+use cargo_metadata::Artifact as CargoArtifact;
 use clap::Parser;
 use console::Term;
 use indicatif::TermLike;
@@ -74,6 +75,7 @@ struct JobQueuer<StdErr> {
     stderr: StdErr,
     stderr_color: bool,
     tracker: Arc<JobStatusTracker>,
+    jobs_queued: u64,
 }
 
 impl<StdErr> JobQueuer<StdErr> {
@@ -91,13 +93,89 @@ impl<StdErr> JobQueuer<StdErr> {
             stderr,
             stderr_color,
             tracker: Arc::new(JobStatusTracker::default()),
+            jobs_queued: 0,
         }
     }
 }
 
 impl<StdErr: io::Write> JobQueuer<StdErr> {
+    fn queue_job_from_case<ProgressIndicatorT>(
+        &mut self,
+        client: &Mutex<Client>,
+        width: usize,
+        ind: ProgressIndicatorT,
+        ignored_cases: &HashSet<String>,
+        package_name: &str,
+        case: &str,
+        binary: &str,
+    ) -> Result<()>
+    where
+        ProgressIndicatorT: ProgressIndicatorScope,
+    {
+        let case_str = format!("{package_name} {case}");
+        let visitor = JobStatusVisitor::new(self.tracker.clone(), case_str, width, ind);
+
+        if ignored_cases.contains(case) {
+            visitor.job_ignored();
+            return Ok(());
+        }
+
+        client.lock().unwrap().add_job(
+            JobDetails {
+                program: binary.into(),
+                arguments: vec!["--exact".into(), "--nocapture".into(), case.into()],
+                environment: vec![],
+                layers: vec![],
+            },
+            Box::new(move |cjid, result| visitor.job_finished(cjid, result)),
+        );
+
+        Ok(())
+    }
+
+    fn queue_jobs_from_artifact<ProgressIndicatorT>(
+        &mut self,
+        client: &Mutex<Client>,
+        width: usize,
+        ind: ProgressIndicatorT,
+        cb: &mut impl FnMut(u64),
+        artifact: CargoArtifact,
+    ) -> Result<bool>
+    where
+        ProgressIndicatorT: ProgressIndicatorScope,
+    {
+        let package_name = artifact.package_id.repr.split(' ').next().unwrap();
+        if let Some(package) = &self.package {
+            if package_name != package {
+                return Ok(false);
+            }
+        }
+
+        let binary = artifact.executable.unwrap().to_string();
+        let ignored_cases: HashSet<_> = get_cases_from_binary(&binary, &Some("--ignored".into()))?
+            .into_iter()
+            .collect();
+
+        for case in get_cases_from_binary(&binary, &self.filter)? {
+            self.jobs_queued += 1;
+            cb(self.jobs_queued);
+
+            self.queue_job_from_case(
+                client,
+                width,
+                ind.clone(),
+                &ignored_cases,
+                package_name,
+                &case,
+                &binary,
+            )?;
+        }
+
+        Ok(true)
+    }
+
     fn queue_jobs_and_wait<ProgressIndicatorT>(
-        self,
+        mut self,
         client: &Mutex<Client>,
         width: usize,
         ind: ProgressIndicatorT,
@@ -106,7 +184,6 @@ impl<StdErr: io::Write> JobQueuer<StdErr> {
     where
         ProgressIndicatorT: ProgressIndicatorScope,
     {
-        let mut total_jobs = 0;
         let mut cargo_build =
             CargoBuild::new(&self.cargo, self.stderr_color, self.package.clone())?;
 
@@ -114,41 +191,8 @@ impl<StdErr: io::Write> JobQueuer<StdErr> {
 
         for artifact in cargo_build.artifact_stream() {
             let artifact = artifact?;
-            let package_name = artifact.package_id.repr.split(' ').next().unwrap();
-            if let Some(package) = &self.package {
-                if package_name != package {
-                    continue;
-                } else {
-                    package_match = true;
-                }
-            }
-            let binary = artifact.executable.unwrap().to_string();
-            let ignored_cases: HashSet<_> =
-                get_cases_from_binary(&binary, &Some("--ignored".into()))?
-                    .into_iter()
-                    .collect();
-            for case in get_cases_from_binary(&binary, &self.filter)? {
-                total_jobs += 1;
-                cb(total_jobs);
-
-                let case_str = format!("{package_name} {case}");
-                let visitor =
-                    JobStatusVisitor::new(self.tracker.clone(), case_str, width, ind.clone());
-
-                if ignored_cases.contains(&case) {
-                    visitor.job_ignored();
-                    continue;
-                }
-                client.lock().unwrap().add_job(
-                    JobDetails {
-                        program: binary.clone(),
-                        arguments: vec!["--exact".into(), "--nocapture".into(), case.clone()],
-                        environment: vec![],
-                        layers: vec![],
-                    },
-                    Box::new(move |cjid, result| visitor.job_finished(cjid, result)),
-                );
-            }
+            package_match |=
+                self.queue_jobs_from_artifact(client, width, ind.clone(), &mut cb, artifact)?;
         }
 
         cargo_build.check_status(self.stderr)?;
