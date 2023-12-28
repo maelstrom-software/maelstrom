13673f3de0908a1462dbae715f699a1b6a1babea
Neal Fachan <neal@fachan.com>
2023-12-28 18:03:57 +0000

    Add some comments and clean up meticulous_client::spec::substitute.

--- crates/meticulous-client/src/spec/substitute.rs
+++ crates/meticulous-client/src/spec/substitute.rs
@@ -1,16 +1,28 @@
+//! Provide [`substitute`] function to do variable substitutions in strings.
 use std::{
     borrow::{Borrow, Cow},
     error, fmt, result,
 };
 
+/// Error type returned by [`substitute`].
 #[derive(Debug, Eq, PartialEq)]
 pub enum Error {
+    /// The input string had an unterminated curly brace.
     UnterminatedBrace,
+
+    /// The input string had a `$` that wasn't followed by `env{` or `prev{`.
     InvalidVariable,
-    UnknownVariable(String),
-    LookupError(String, String),
+
+    /// The input string had a `$env{` or `$prev{` substitution for `var` where the lookup closure
+    /// couldn't find `var`.
+    UnknownVariable { var: String },
+
+    /// The input string had a `$env{` substitution for `var` where the lookup closure returned an
+    /// error, `err`.
+    LookupError { var: String, err: String },
 }
 
+/// Return type of [`substitute`].
 pub type Result<T> = result::Result<T, Error>;
 
 impl fmt::Display for Error {
@@ -20,8 +32,8 @@ impl fmt::Display for Error {
             Error::InvalidVariable => {
                 write!(f, r#"$ must be followed by "$", "env{{", or "prev{{""#)
             }
-            Error::UnknownVariable(var) => write!(f, "unknown variable {var:?}"),
-            Error::LookupError(var, err) => write!(f, "error lookup up variable {var:?}: {err}"),
+            Error::UnknownVariable { var } => write!(f, "unknown variable {var:?}"),
+            Error::LookupError { var, err } => write!(f, "error lookup up variable {var:?}: {err}"),
         }
     }
 }
@@ -45,7 +57,10 @@ where
         (var, None)
     };
 
-    let val = lookup(var).map_err(|err| Error::LookupError(var.to_string(), format!("{err}")))?;
+    let val = lookup(var).map_err(|err| Error::LookupError {
+        var: var.to_string(),
+        err: format!("{err}"),
+    })?;
     match (val, default) {
         (None, Some(default)) => {
             result.push_str(default);
@@ -54,7 +69,9 @@ where
             result.push_str(default);
         }
         (None, _) => {
-            return Err(Error::UnknownVariable(var.to_string()));
+            return Err(Error::UnknownVariable {
+                var: var.to_string(),
+            });
         }
         (Some(val), _) => {
             result.push_str(val.borrow());
@@ -64,6 +81,22 @@ where
     Ok(())
 }
 
+/// Substitute `$env{var}` and `$prev{var}` uses in a string.
+///
+/// `$env{var}` is used to evaluate environment variables. The `env_lookup` closure provides the
+/// means for looking up environment variables. It is allowed to return an error to indicate an
+/// actual error, like the variable not being valid UTF-8. In this case, the error will be wrapped
+/// in [`Error::LookupError`]. It is also allowed to return `None` to indicate there is no such
+/// variable.
+///
+/// `$prev{var}` is used to for previous values. This could be useful adding something on to a
+/// environment variable like `PATH`, where the new value could be `$prev{PATH}:/some/new/path`.
+/// The `prev_lookup` closure provides the means for looking up previous variables. It is not
+/// allowed to return an error, but it can return `None` to indicate no such variable.
+///
+/// If either variable lookup closure returns `None`, this is treated as an error. An error of type
+/// [`Error::UnknownVariable`] will be returned. If the caller wants to treat missing variables as
+/// empty strings, they should do so in the loopup closures.
 pub fn substitute<'a, EnvLookupT, EnvVarT, PrevLookupT, PrevVarT>(
     input: &'a str,
     env_lookup: EnvLookupT,
@@ -78,34 +111,32 @@ where
     let mut iter = input.split('$').peekable();
     let first = iter.next().unwrap();
 
-    if iter.peek().is_some() {
-        let mut result = first.to_string();
-
-        while let Some(subst_str) = iter.next() {
-            if subst_str.is_empty() {
-                match iter.next() {
-                    None => {
-                        return Err(Error::InvalidVariable);
-                    }
-                    Some(next) => {
-                        result.push('$');
-                        result.push_str(next);
-                    }
-                }
-            } else if let Some(rest) = subst_str.strip_prefix("env{") {
-                handle_variable(&mut result, rest, &env_lookup)?;
-            } else if let Some(rest) = subst_str.strip_prefix("prev{") {
-                handle_variable(&mut result, rest, |k| Ok(prev_lookup(k)))?;
-            } else {
-                return Err(Error::InvalidVariable);
-            }
-        }
-
-        Ok(result.into())
-    } else {
+    if iter.peek().is_none() {
         // There was no dollar sign. Just return the original string.
-        Ok(input.into())
+        return Ok(input.into());
     }
+
+    let mut result = first.to_string();
+    while let Some(subst_str) = iter.next() {
+        if subst_str.is_empty() {
+            match iter.next() {
+                None => {
+                    return Err(Error::InvalidVariable);
+                }
+                Some(next) => {
+                    result.push('$');
+                    result.push_str(next);
+                }
+            }
+        } else if let Some(rest) = subst_str.strip_prefix("env{") {
+            handle_variable(&mut result, rest, &env_lookup)?;
+        } else if let Some(rest) = subst_str.strip_prefix("prev{") {
+            handle_variable(&mut result, rest, |k| Ok(prev_lookup(k)))?;
+        } else {
+            return Err(Error::InvalidVariable);
+        }
+    }
+    Ok(result.into())
 }
 
 #[cfg(test)]
@@ -280,7 +311,9 @@ mod tests {
     fn unknown_variable() {
         assert_eq!(
             substitute("$env{UNKNOWN}", env_lookup, prev_lookup).unwrap_err(),
-            Error::UnknownVariable(string!("UNKNOWN")),
+            Error::UnknownVariable {
+                var: string!("UNKNOWN")
+            },
         );
     }
 
@@ -288,7 +321,10 @@ mod tests {
     fn lookup_error() {
         assert_eq!(
             substitute("$env{ERROR}", env_lookup, prev_lookup).unwrap_err(),
-            Error::LookupError(string!("ERROR"), string!("an error occurred")),
+            Error::LookupError {
+                var: string!("ERROR"),
+                err: string!("an error occurred")
+            },
         );
     }
 }
