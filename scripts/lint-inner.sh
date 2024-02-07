429241214604b7ccd5395e9b1b8e22e447a3af03
nfachan <neal@fachan.com>
2024-02-07 09:36:00 +0000

    Remove "crates/" from package specification.

--- scripts/lint-inner.sh
+++ scripts/lint-inner.sh
@@ -3,5 +3,5 @@
 set -ex
 
 cargo fmt --check
-cargo build --lib --package crates/maelstrom-web --target wasm32-unknown-unknown
+cargo build --lib --package maelstrom-web --target wasm32-unknown-unknown
 cargo clippy
