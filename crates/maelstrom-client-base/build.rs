4f521d1fbca3bcc3d5abe0945210059a7f004bc5
Remi Bernotavicius <remi@abort.cc>
2024-02-26 12:21:28 -0800

    Refactor the maelstrom-client-base build script a bit

--- crates/maelstrom-client-base/build.rs
+++ crates/maelstrom-client-base/build.rs
@@ -7,71 +7,63 @@ const INTO_RESULT: [&str; 6] = [
     "GetJobStateCountsResponse",
 ];
 
+const ENUM_PROTO: [(&str, &str); 4] = [
+    ("JobDevice", "maelstrom_base::JobDevice"),
+    ("JobMountFsType", "maelstrom_base::JobMountFsType"),
+    ("ArtifactType", "maelstrom_base::ArtifactType"),
+    ("JobOutcomeCompleted.status", "maelstrom_base::JobStatus"),
+];
+
+const MSG_PROTO: [(&str, &str, &str); 7] = [
+    ("JobMount", "maelstrom_base::JobMount", ""),
+    ("JobSpec", "maelstrom_base::JobSpec", ""),
+    ("ContainerImage", "maelstrom_container::ContainerImage", ""),
+    (
+        "OciImageConfiguration",
+        "maelstrom_container::ImageConfiguration",
+        "",
+    ),
+    ("OciConfig", "maelstrom_container::Config", ""),
+    ("OciRootFs", "maelstrom_container::RootFs", ""),
+    ("JobEffects", "maelstrom_base::JobEffects", "option_all"),
+];
+
+const FIELD_ATTR: [(&str, &str); 4] = [
+    ("ContainerImage.config", "option"),
+    ("OciImageConfiguration.architecture", "option"),
+    ("OciImageConfiguration.os", "option"),
+    ("OciImageConfiguration.rootfs", "option"),
+];
+
 fn main() {
     let mut b = tonic_build::configure();
     for resp in INTO_RESULT {
         b = b.message_attribute(resp, "#[derive(maelstrom_macro::IntoResult)]");
     }
 
-    let enum_proto = |b: &mut tonic_build::Builder, name, remote| {
-        *b = b.clone().enum_attribute(
+    for (name, other_type) in ENUM_PROTO {
+        b = b.enum_attribute(
             name,
             format!(
                 "#[derive(maelstrom_macro::IntoProtoBuf, maelstrom_macro::TryFromProtoBuf)] \
-                 #[proto(other_type = {remote}, remote)]"
+                 #[proto(other_type = {other_type}, remote)]"
             ),
         );
-    };
-    enum_proto(&mut b, "JobDevice", "maelstrom_base::JobDevice");
-    enum_proto(&mut b, "JobMountFsType", "maelstrom_base::JobMountFsType");
-    enum_proto(&mut b, "ArtifactType", "maelstrom_base::ArtifactType");
+    }
 
-    let message_proto = |b: &mut tonic_build::Builder, name, remote| {
-        *b = b.clone().message_attribute(
+    for (name, other_type, extra) in MSG_PROTO {
+        b = b.message_attribute(
             name,
             format!(
                 "#[derive(maelstrom_macro::IntoProtoBuf, maelstrom_macro::TryFromProtoBuf)] \
-                 #[proto(other_type = {remote}, remote)]"
+                 #[proto(other_type = {other_type}, remote, {extra})]"
             ),
         );
-    };
-    message_proto(&mut b, "JobMount", "maelstrom_base::JobMount");
-    message_proto(&mut b, "JobSpec", "maelstrom_base::JobSpec");
+    }
 
-    b = b.message_attribute(
-        "ContainerImage",
-        "#[derive(maelstrom_macro::TryFromProtoBuf, maelstrom_macro::IntoProtoBuf)] \
-         #[proto(other_type = maelstrom_container::ContainerImage, remote)]",
-    );
-    b = b.field_attribute("ContainerImage.config", "#[proto(option)]");
-    b = b.message_attribute(
-        "OciImageConfiguration",
-        "#[derive(maelstrom_macro::TryFromProtoBuf, maelstrom_macro::IntoProtoBuf)] \
-         #[proto(other_type = maelstrom_container::ImageConfiguration, remote)]",
-    );
-    b = b.field_attribute("OciImageConfiguration.architecture", "#[proto(option)]");
-    b = b.field_attribute("OciImageConfiguration.os", "#[proto(option)]");
-    b = b.field_attribute("OciImageConfiguration.rootfs", "#[proto(option)]");
-    b = b.message_attribute(
-        "OciConfig",
-        "#[derive(maelstrom_macro::TryFromProtoBuf, maelstrom_macro::IntoProtoBuf)] \
-         #[proto(other_type = maelstrom_container::Config, remote)]",
-    );
-    b = b.message_attribute(
-        "OciRootFs",
-        "#[derive(maelstrom_macro::TryFromProtoBuf, maelstrom_macro::IntoProtoBuf)] \
-         #[proto(other_type = maelstrom_container::RootFs, remote)]",
-    );
-    b = b.enum_attribute(
-        "JobOutcomeCompleted.status",
-        "#[derive(maelstrom_macro::TryFromProtoBuf, maelstrom_macro::IntoProtoBuf)] \
-         #[proto(other_type = maelstrom_base::JobStatus, remote)]",
-    );
-    b = b.message_attribute(
-        "JobEffects",
-        "#[derive(maelstrom_macro::TryFromProtoBuf, maelstrom_macro::IntoProtoBuf)] \
-         #[proto(other_type = maelstrom_base::JobEffects, remote, option_all)]",
-    );
+    for (name, attrs) in FIELD_ATTR {
+        b = b.field_attribute(name, &format!("#[proto({attrs})]"));
+    }
 
     b.compile(&["src/items.proto"], &["src/"]).unwrap();
 }
