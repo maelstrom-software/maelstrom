7cf41430337ce9755ab9b7452e1a9bfeb70a1e3f
Neal Fachan <neal@fachan.com>
2024-03-26 14:14:11 -0700

    Use clap's derive feature for cargo-maelstrom's extra command-line options.

--- crates/cargo-maelstrom/src/main.rs
+++ crates/cargo-maelstrom/src/main.rs
@@ -3,66 +3,69 @@ use cargo_maelstrom::{
     config::Config, main_app_new, progress::DefaultProgressDriver, ListAction, Logger, MainAppDeps,
 };
 use cargo_metadata::Metadata as CargoMetadata;
-use clap::{command, Arg, ArgAction, ArgGroup, Command};
+use clap::{command, Args, Command, FromArgMatches as _};
 use console::Term;
 use maelstrom_base::Timeout;
 use maelstrom_client::ClientBgProcess;
 use maelstrom_util::process::ExitCode;
 use std::{env, io::IsTerminal as _, process};
 
+#[derive(Args)]
+#[command(next_help_heading = "Test Selection Options")]
+struct ExtraCommandLineOptions {
+    #[arg(
+        long,
+        short = 'i',
+        value_name = "FILTER-EXPRESSION",
+        default_value = "all",
+        help = "Only include tests which match the given filter. Can be specified multiple times."
+    )]
+    include: Vec<String>,
+
+    #[arg(
+        long,
+        short = 'x',
+        value_name = "FILTER-EXPRESSION",
+        help = "Only include tests which don't match the given filter. Can be specified multiple times."
+    )]
+    exclude: Vec<String>,
+
+    #[command(flatten)]
+    list: ListOptions,
+}
+
+#[derive(Args)]
+#[group(multiple = false)]
+#[command(next_help_heading = "List Options")]
+struct ListOptions {
+    #[arg(
+        long = "list-tests",
+        visible_alias = "list",
+        help = "Instead of running tests, print the tests that would have been run. \
+            May require building test binaries."
+    )]
+    tests: bool,
+
+    #[arg(
+        long = "list-binaries",
+        help = "Instead of running tests, print the test binaries of those tests that would \
+            have been run."
+    )]
+    binaries: bool,
+
+    #[arg(
+        long = "list-packages",
+        help = "Instead of running tests, print the packages of those tests that would \
+            have been run."
+    )]
+    packages: bool,
+}
+
 /// The main function for the client. This should be called on a task of its own. It will return
 /// when a signal is received or when all work has been processed by the broker.
 pub fn main() -> Result<ExitCode> {
     let add_more_command_line_options = |command: Command| {
-        command
-            .next_help_heading("Test Selection Options")
-            .arg(
-                Arg::new("include")
-                    .long("include")
-                    .short('i')
-                    .value_name("FILTER-EXPRESSION")
-                    .action(ArgAction::Append)
-                    .help("Only include tests which match the given filter. Can be specified multiple times.")
-            )
-            .arg(
-                Arg::new("exclude")
-                    .long("exclude")
-                    .short('x')
-                    .value_name("FILTER-EXPRESSION")
-                    .action(ArgAction::Append)
-                    .help("Only include tests which don't match the given filter. Can be specified multiple times.")
-            )
-            .next_help_heading("List Options")
-            .arg(
-                Arg::new("list-tests")
-                    .long("list-tests")
-                    .visible_alias("list")
-                    .action(ArgAction::SetTrue)
-                    .help(
-                        "Instead of running tests, print the tests that would have been run. \
-                        May require building test binaries."
-                    )
-            )
-            .arg(
-                Arg::new("list-binaries")
-                    .long("list-binaries")
-                    .action(ArgAction::SetTrue)
-                    .help(
-                        "Instead of running tests, print the test binaries of those tests \
-                        that would have been run."
-                    )
-            )
-            .arg(
-                Arg::new("list-packages")
-                    .long("list-packages")
-                    .action(ArgAction::SetTrue)
-                    .help(
-                        "Instead of running tests, print the packages of those tests \
-                        that would have been run."
-                    )
-            )
-            .group(
-                ArgGroup::new("list").args(["list-tests", "list-binaries", "list-packages"]))
+        ExtraCommandLineOptions::augment_args(command)
     };
 
     let mut args = Vec::from_iter(env::args());
@@ -70,7 +73,7 @@ pub fn main() -> Result<ExitCode> {
         args.remove(1);
     }
 
-    let (config, mut args): (Config, _) = maelstrom_util::config::new_config2(
+    let (config, args): (Config, _) = maelstrom_util::config::new_config2(
         command!(),
         "maelstrom/cargo-maelstrom",
         "CARGO_MAELSTROM",
@@ -78,20 +81,12 @@ pub fn main() -> Result<ExitCode> {
         args,
     )?;
 
-    let include = if let Some(values) = args.remove_many::<String>("include") {
-        Vec::from_iter(values)
-    } else {
-        vec!["all".to_string()]
-    };
-    let exclude = if let Some(values) = args.remove_many::<String>("exclude") {
-        Vec::from_iter(values)
-    } else {
-        vec![]
-    };
+    let extra_options = ExtraCommandLineOptions::from_arg_matches(&args)?;
+
     let list_action = match (
-        args.remove_one::<bool>("list-tests").unwrap(),
-        args.remove_one::<bool>("list-binaries").unwrap(),
-        args.remove_one::<bool>("list-packages").unwrap(),
+        extra_options.list.tests,
+        extra_options.list.binaries,
+        extra_options.list.packages,
     ) {
         (true, _, _) => Some(ListAction::ListTests),
         (_, true, _) => Some(ListAction::ListBinaries),
@@ -113,8 +108,8 @@ pub fn main() -> Result<ExitCode> {
     let deps = MainAppDeps::new(
         bg_proc,
         "cargo".into(),
-        include,
-        exclude,
+        extra_options.include,
+        extra_options.exclude,
         list_action,
         std::io::stderr(),
         std::io::stderr().is_terminal(),
