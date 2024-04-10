adb0fd8bd80ea698b3712f558d99015da32c7df1
Philip Taron <philip.taron@gmail.com>
2024-04-09 17:07:04 -0700

    nix: finally a successful build with a little postUnpack step

--- package.nix
+++ package.nix
@@ -23,25 +23,33 @@ let
     src = path ./.;
     filter = srcFilter;
   };
+
+  self = buildPackage {
+    # NOTE: we need to force lld otherwise rust-lld is not found for wasm32 target
+    env.CARGO_TARGET_WASM32_UNKNOWN_UNKNOWN_LINKER = "lld";
+
+    pname = "all";
+    inherit src;
+
+    strictDeps = true;
+
+    # maelstrom-web has a WASM build step that shells out to `cargo`, and needs to be informed about
+    # being inside the Nix build sandbox.
+    postUnpack = ''
+      mkdir source/crates/maelstrom-web/.cargo
+      ln -s ${self.cargoVendorDir}/config.toml source/crates/maelstrom-web/.cargo/config.toml
+    '';
+
+    nativeBuildInputs = [
+      binaryen
+      pkg-config
+      llvmPackages.bintools
+      protobuf
+    ];
+
+    buildInputs = [ openssl ] ++ optionals stdenv.isDarwin [ libiconv ];
+
+    doCheck = false;
+  };
 in
-
-buildPackage {
-  # NOTE: we need to force lld otherwise rust-lld is not found for wasm32 target
-  env.CARGO_TARGET_WASM32_UNKNOWN_UNKNOWN_LINKER = "lld";
-
-  pname = "all";
-  inherit src;
-
-  strictDeps = true;
-
-  nativeBuildInputs = [
-    binaryen
-    pkg-config
-    llvmPackages.bintools
-    protobuf
-  ];
-
-  buildInputs = [ openssl ] ++ optionals stdenv.isDarwin [ libiconv ];
-
-  doCheck = false;
-}
+self
