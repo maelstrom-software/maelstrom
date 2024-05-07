9f01d5b35878ed8b196856ebfeb7976f85169cd2
Neal Fachan <neal@fachan.com>
2024-05-07 10:22:31 -0700

    Add a lot of tests of the TestListingStore.
    As part of this, change the behavior so that older and newer versions are
    accepted and then turned into empty TestListings.

--- crates/cargo-maelstrom/src/test_listing.rs
+++ crates/cargo-maelstrom/src/test_listing.rs
@@ -1,11 +1,14 @@
 use crate::pattern;
-use anyhow::{anyhow, Result};
+pub use crate::pattern::ArtifactKind;
+use anyhow::{anyhow, bail, Result};
 use cargo_metadata::{Artifact as CargoArtifact, Package as CargoPackage, Target as CargoTarget};
 use maelstrom_client::StateDir;
 use maelstrom_util::{
     fs::Fs,
     root::{Root, RootBuf},
 };
+use num_derive::FromPrimitive;
+use num_traits::FromPrimitive as _;
 use serde::{Deserialize, Serialize};
 use serde_repr::{Deserialize_repr, Serialize_repr};
 use serde_with::{serde_as, FromInto};
@@ -13,8 +16,6 @@ use std::{
     collections::{BTreeMap, HashMap, HashSet},
     path::Path,
 };
-
-pub use crate::pattern::ArtifactKind;
 
 /*  _
  * (_)_ __    _ __ ___   ___ _ __ ___   ___  _ __ _   _
@@ -125,7 +126,7 @@ impl TestListing {
  *  FIGLET: on disk
  */
 
-#[derive(Eq, PartialEq, Serialize_repr, Deserialize_repr)]
+#[derive(Deserialize_repr, Eq, FromPrimitive, PartialEq, Serialize_repr)]
 #[repr(u32)]
 enum OnDiskTestListingVersion {
     V1 = 1,
@@ -196,7 +197,11 @@ impl From<TestListing> for OnDiskTestListing {
                                     (
                                         artifact_key,
                                         OnDiskArtifactCases {
-                                            cases: artifact.cases.into_iter().collect(),
+                                            cases: {
+                                                let mut cases = Vec::from_iter(artifact.cases);
+                                                cases.sort();
+                                                cases
+                                            },
                                         },
                                     )
                                 })
@@ -248,9 +253,15 @@ impl From<OnDiskTestListing> for TestListing {
  */
 
 pub trait TestListingStoreDeps {
-    fn read_to_string_if_exists(&self, path: impl AsRef<Path>) -> Result<Option<String>>;
-    fn create_dir_all(&self, path: impl AsRef<Path>) -> Result<()>;
-    fn write(&self, path: impl AsRef<Path>, contents: impl AsRef<[u8]>) -> Result<()>;
+    fn read_to_string_if_exists(&self, path: impl AsRef<Path>) -> Result<Option<String>> {
+        unimplemented!("{:?}", path.as_ref());
+    }
+    fn create_dir_all(&self, path: impl AsRef<Path>) -> Result<()> {
+        unimplemented!("{:?}", path.as_ref());
+    }
+    fn write(&self, path: impl AsRef<Path>, contents: impl AsRef<[u8]>) -> Result<()> {
+        unimplemented!("{:?} {:?}", path.as_ref(), contents.as_ref());
+    }
 }
 
 impl TestListingStoreDeps for Fs {
@@ -274,41 +285,356 @@ pub struct TestListingStore<DepsT = Fs> {
     test_listing_file: RootBuf<TestListingFile>,
 }
 
+const MISSING_VERSION: &str = "missing version";
+const VERSION_NOT_AN_INTEGER: &str = "version field is not an integer";
+const TEST_LISTING_FILE: &str = "test-listing.toml";
+
 impl<DepsT: TestListingStoreDeps> TestListingStore<DepsT> {
     pub fn new(deps: DepsT, state_dir: impl AsRef<Root<StateDir>>) -> Self {
         Self {
             deps,
-            test_listing_file: state_dir.as_ref().join("test-listing.toml"),
+            test_listing_file: state_dir.as_ref().join(TEST_LISTING_FILE),
         }
     }
 
     pub fn load(&self) -> Result<TestListing> {
-        if let Some(contents) = self
+        let Some(contents) = self
             .deps
             .read_to_string_if_exists(&self.test_listing_file)?
-        {
-            let mut table: toml::Table = toml::from_str(&contents)?;
-            let version: OnDiskTestListingVersion = table
-                .remove("version")
-                .ok_or(anyhow!("missing version"))?
-                .try_into()?;
-            if version != OnDiskTestListingVersion::V1 {
-                Ok(Default::default())
-            } else {
+        else {
+            return Ok(Default::default());
+        };
+        let mut table: toml::Table = toml::from_str(&contents)?;
+        let version = table.remove("version").ok_or(anyhow!(MISSING_VERSION))?;
+        let Some(version) = version.as_integer() else {
+            bail!(VERSION_NOT_AN_INTEGER);
+        };
+        match OnDiskTestListingVersion::from_i64(version) {
+            None => Ok(Default::default()),
+            Some(OnDiskTestListingVersion::V1) => {
                 Ok(toml::from_str::<OnDiskTestListing>(&contents)?.into())
             }
-        } else {
-            Ok(Default::default())
         }
     }
 
     pub fn save(&self, job_listing: TestListing) -> Result<()> {
-        if let Some(parent) = self.test_listing_file.parent() {
-            self.deps.create_dir_all(parent)?;
-        }
+        self.deps
+            .create_dir_all(self.test_listing_file.parent().unwrap())?;
         self.deps.write(
             &self.test_listing_file,
             toml::to_string_pretty::<OnDiskTestListing>(&job_listing.into())?,
         )
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use indoc::indoc;
+    use maelstrom_util::ext::OptionExt as _;
+    use std::{cell::RefCell, rc::Rc, str};
+
+    #[test]
+    fn load_passes_proper_path() {
+        struct Deps;
+        impl TestListingStoreDeps for Deps {
+            fn read_to_string_if_exists(&self, path: impl AsRef<Path>) -> Result<Option<String>> {
+                assert_eq!(
+                    path.as_ref().to_str().unwrap(),
+                    format!("path/to/state/{TEST_LISTING_FILE}")
+                );
+                Ok(None)
+            }
+        }
+        let _ = TestListingStore::new(Deps, RootBuf::new("path/to/state".into()));
+    }
+
+    #[test]
+    fn error_reading_in_load_propagates_error() {
+        struct Deps;
+        impl TestListingStoreDeps for Deps {
+            fn read_to_string_if_exists(&self, _: impl AsRef<Path>) -> Result<Option<String>> {
+                Err(anyhow!("error!"))
+            }
+        }
+        let store = TestListingStore::new(Deps, RootBuf::new("".into()));
+        assert_eq!(store.load().unwrap_err().to_string(), "error!");
+    }
+
+    #[test]
+    fn load_of_nonexistent_file_gives_default_listing() {
+        struct Deps;
+        impl TestListingStoreDeps for Deps {
+            fn read_to_string_if_exists(&self, _: impl AsRef<Path>) -> Result<Option<String>> {
+                Ok(None)
+            }
+        }
+        let store = TestListingStore::new(Deps, RootBuf::new("".into()));
+        assert_eq!(store.load().unwrap(), TestListing::default());
+    }
+
+    #[test]
+    fn load_of_file_with_invalid_toml_gives_toml_parse_error() {
+        struct Deps;
+        impl TestListingStoreDeps for Deps {
+            fn read_to_string_if_exists(&self, _: impl AsRef<Path>) -> Result<Option<String>> {
+                Ok(Some(r#""garbage": { "foo", "bar" }"#.into()))
+            }
+        }
+        let store = TestListingStore::new(Deps, RootBuf::new("".into()));
+        let error = store.load().unwrap_err().to_string();
+        assert!(error.starts_with("TOML parse error"));
+    }
+
+    #[test]
+    fn load_of_empty_file_gives_missing_version_error() {
+        struct Deps;
+        impl TestListingStoreDeps for Deps {
+            fn read_to_string_if_exists(&self, _: impl AsRef<Path>) -> Result<Option<String>> {
+                Ok(Some("foo = 3\n".into()))
+            }
+        }
+        let store = TestListingStore::new(Deps, RootBuf::new("".into()));
+        assert_eq!(store.load().unwrap_err().to_string(), MISSING_VERSION);
+    }
+
+    #[test]
+    fn load_of_file_without_version_gives_missing_version_error() {
+        struct Deps;
+        impl TestListingStoreDeps for Deps {
+            fn read_to_string_if_exists(&self, _: impl AsRef<Path>) -> Result<Option<String>> {
+                Ok(Some("foo = 3\n".into()))
+            }
+        }
+        let store = TestListingStore::new(Deps, RootBuf::new("".into()));
+        assert_eq!(store.load().unwrap_err().to_string(), MISSING_VERSION);
+    }
+
+    #[test]
+    fn load_of_file_with_non_integer_version_gives_version_not_an_integer_error() {
+        struct Deps;
+        impl TestListingStoreDeps for Deps {
+            fn read_to_string_if_exists(&self, _: impl AsRef<Path>) -> Result<Option<String>> {
+                Ok(Some("version = \"v1\"\n".into()))
+            }
+        }
+        let store = TestListingStore::new(Deps, RootBuf::new("".into()));
+        assert_eq!(
+            store.load().unwrap_err().to_string(),
+            VERSION_NOT_AN_INTEGER
+        );
+    }
+
+    #[test]
+    fn load_of_file_with_old_version_gives_default_listing() {
+        struct Deps;
+        impl TestListingStoreDeps for Deps {
+            fn read_to_string_if_exists(&self, _: impl AsRef<Path>) -> Result<Option<String>> {
+                Ok(Some("version = 0\nfoo = \"bar\"\n".into()))
+            }
+        }
+        let store = TestListingStore::new(Deps, RootBuf::new("".into()));
+        assert_eq!(store.load().unwrap(), TestListing::default());
+    }
+
+    #[test]
+    fn load_of_file_with_newer_version_gives_default_listing() {
+        struct Deps;
+        impl TestListingStoreDeps for Deps {
+            fn read_to_string_if_exists(&self, _: impl AsRef<Path>) -> Result<Option<String>> {
+                Ok(Some("version = 1000000\nfoo = \"bar\"\n".into()))
+            }
+        }
+        let store = TestListingStore::new(Deps, RootBuf::new("".into()));
+        assert_eq!(store.load().unwrap(), TestListing::default());
+    }
+
+    #[test]
+    fn load_of_file_with_correct_version_gives_deserialized_listing() {
+        struct Deps;
+        impl TestListingStoreDeps for Deps {
+            fn read_to_string_if_exists(&self, _: impl AsRef<Path>) -> Result<Option<String>> {
+                Ok(Some(
+                    indoc! {r#"
+                        version = 1
+
+                        [[package1.artifacts]]
+                        name = "package1"
+                        kind = "Library"
+                        cases = [
+                            "case1",
+                            "case2",
+                        ]
+                    "#}
+                    .into(),
+                ))
+            }
+        }
+        let store = TestListingStore::new(Deps, RootBuf::new("".into()));
+        let expected = TestListing {
+            packages: HashMap::from_iter([(
+                "package1".into(),
+                Package {
+                    artifacts: HashMap::from_iter([(
+                        ArtifactKey {
+                            name: "package1".into(),
+                            kind: ArtifactKind::Library,
+                        },
+                        Artifact {
+                            cases: HashSet::from_iter(["case1".into(), "case2".into()]),
+                        },
+                    )]),
+                },
+            )]),
+        };
+        assert_eq!(store.load().unwrap(), expected);
+    }
+
+    #[test]
+    fn load_of_file_with_correct_version_but_bad_toml_gives_toml_parse_error() {
+        struct Deps;
+        impl TestListingStoreDeps for Deps {
+            fn read_to_string_if_exists(&self, _: impl AsRef<Path>) -> Result<Option<String>> {
+                Ok(Some(
+                    indoc! {r#"
+                        version = 1
+
+                        [[frob.blah]]
+                        foo = "package1"
+                        bar = "Library"
+                        baz = [
+                            "case1",
+                            "case2",
+                        ]
+                    "#}
+                    .into(),
+                ))
+            }
+        }
+        let store = TestListingStore::new(Deps, RootBuf::new("".into()));
+        let error = store.load().unwrap_err().to_string();
+        assert!(error.starts_with("TOML parse error"));
+    }
+
+    #[test]
+    fn error_creating_dir_in_save_propagates_error() {
+        struct Deps;
+        impl TestListingStoreDeps for Deps {
+            fn create_dir_all(&self, _: impl AsRef<Path>) -> Result<()> {
+                Err(anyhow!("error!"))
+            }
+        }
+        let store = TestListingStore::new(Deps, RootBuf::new("state".into()));
+        assert_eq!(
+            store.save(TestListing::default()).unwrap_err().to_string(),
+            "error!"
+        );
+    }
+
+    #[test]
+    fn error_writing_in_save_propagates_error() {
+        struct Deps;
+        impl TestListingStoreDeps for Deps {
+            fn create_dir_all(&self, _: impl AsRef<Path>) -> Result<()> {
+                Ok(())
+            }
+            fn write(&self, _: impl AsRef<Path>, _: impl AsRef<[u8]>) -> Result<()> {
+                Err(anyhow!("error!"))
+            }
+        }
+        let store = TestListingStore::new(Deps, RootBuf::new("state".into()));
+        assert_eq!(
+            store.save(TestListing::default()).unwrap_err().to_string(),
+            "error!"
+        );
+    }
+
+    #[derive(Default)]
+    struct LoggingDeps {
+        create_dir_all: Option<String>,
+        write: Option<(String, String)>,
+    }
+
+    impl TestListingStoreDeps for Rc<RefCell<LoggingDeps>> {
+        fn create_dir_all(&self, path: impl AsRef<Path>) -> Result<()> {
+            Ok(self
+                .borrow_mut()
+                .create_dir_all
+                .replace(path.as_ref().to_str().unwrap().to_string())
+                .assert_is_none())
+        }
+        fn write(&self, path: impl AsRef<Path>, contents: impl AsRef<[u8]>) -> Result<()> {
+            Ok(self
+                .borrow_mut()
+                .write
+                .replace((
+                    path.as_ref().to_str().unwrap().to_string(),
+                    str::from_utf8(contents.as_ref()).unwrap().to_string(),
+                ))
+                .assert_is_none())
+        }
+    }
+
+    #[test]
+    fn save_creates_parent_directory() {
+        let deps = Rc::new(RefCell::new(LoggingDeps::default()));
+        let store = TestListingStore::new(deps.clone(), RootBuf::new("maelstrom/state/".into()));
+        store.save(TestListing::default()).unwrap();
+        assert_eq!(deps.borrow().create_dir_all, Some("maelstrom/state".into()));
+    }
+
+    #[test]
+    fn save_of_default() {
+        let deps = Rc::new(RefCell::new(LoggingDeps::default()));
+        let store = TestListingStore::new(deps.clone(), RootBuf::new("maelstrom/state/".into()));
+        store.save(TestListing::default()).unwrap();
+        assert_eq!(
+            deps.borrow().write,
+            Some((
+                format!("maelstrom/state/{TEST_LISTING_FILE}"),
+                "version = 1\n".into()
+            ))
+        );
+    }
+
+    #[test]
+    fn save_of_simple_listing() {
+        let deps = Rc::new(RefCell::new(LoggingDeps::default()));
+        let store = TestListingStore::new(deps.clone(), RootBuf::new("maelstrom/state/".into()));
+        let listing = TestListing {
+            packages: HashMap::from_iter([(
+                "package1".into(),
+                Package {
+                    artifacts: HashMap::from_iter([(
+                        ArtifactKey {
+                            name: "package1".into(),
+                            kind: ArtifactKind::Library,
+                        },
+                        Artifact {
+                            cases: HashSet::from_iter(["case1".into(), "case2".into()]),
+                        },
+                    )]),
+                },
+            )]),
+        };
+        store.save(listing).unwrap();
+        assert_eq!(
+            deps.borrow().write,
+            Some((
+                format!("maelstrom/state/{TEST_LISTING_FILE}"),
+                indoc! {r#"
+                    version = 1
+
+                    [[package1.artifacts]]
+                    name = "package1"
+                    kind = "Library"
+                    cases = [
+                        "case1",
+                        "case2",
+                    ]
+                "#}
+                .into()
+            ))
+        );
     }
 }
