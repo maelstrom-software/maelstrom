de22d65ae9d896eb5919a13b8c11ba827ee3673d
Remi Bernotavicius <remi@abort.cc>
2024-05-29 15:23:51 -0700

    python: Simplify collect_tests.py
    Turns out the thing I was looking for is called "nodeid"

--- crates/maelstrom-pytest/src/py/collect_tests.py
+++ crates/maelstrom-pytest/src/py/collect_tests.py
@@ -25,52 +25,10 @@ def collect_pytest_tests() -> List[pytest.Item]:
     return plugin.items
 
 
-def build_test_name(start: pytest.Item) -> str:
-    base_parent = None
-
-    def get_info(item: PytestNode) -> Optional[Tuple[str, str]]:
-        if hasattr(item, "reportinfo"):
-            (file, _, case) = item.reportinfo()
-            return (str(file), case)
-        else:
-            return None
-
-    computed_file = None
-    computed_case = None
-
-    i: PytestNode = start
-    while True:
-        i_info = get_info(i)
-        if i_info is not None:
-            (file, case_) = i_info
-            computed_file = file
-
-            if computed_case is None:
-                computed_case = case_.replace(".", "::")
-            else:
-                computed_case = case_.replace(".", "::") + "::" + computed_case
-
-            if not file.endswith(".py"):
-                computed_case = os.path.basename(file) + "::" + computed_case
-
-            if file.endswith(".py"):
-                break
-        assert i.parent is not None
-        i = i.parent
-    assert computed_file is not None
-    assert computed_case is not None
-
-    base_parent = i
-
-    computed_file = os.path.relpath(computed_file, ".")
-    return f"{computed_file}::{computed_case}"
-
-
 def main() -> None:
     tests = collect_pytest_tests()
     for item in tests:
-        test_name = build_test_name(item)
-        print(test_name)
+        print(item.nodeid)
 
 
 if __name__ == "__main__":
