9853a9f4d9123fed7d384ceecd03d501794d40ad
Remi Bernotavicius <remi@abort.cc>
2024-08-21 15:51:09 -0700

    Update py/run_job.py

--- py/run_job.py
+++ py/run_job.py
@@ -1,20 +1,26 @@
-#!/usr/bin/env nix-shell
-#! nix-shell -i python3 -p python3
+#!/usr/bin/env python
 
 import os
 import sys
 
-from maelstrom_client import Client, ImageSpec
+from maelstrom_client import Client, ImageSpec, ContainerSpec, ContainerRef, JobSpec
 
 def main():
     prog=sys.argv[1]
     arguments=sys.argv[2:]
 
-    client = Client()
+    client = Client(slots=4)
 
-    image = ImageSpec(name="alpine", tag="latest", use_layers=True)
-    job = client.run_job(prog, arguments, image=image)
-    result = job.result()
+    image = ImageSpec(name="docker://alpine", use_layers=True)
+    container = ContainerSpec(working_directory="/", image=image)
+    spec = JobSpec(
+        container=ContainerRef(inline=container),
+        program=prog,
+        arguments=arguments,
+    )
+    stream = client.run_job(spec)
+    for status in stream:
+        result = status.completed
 
     if result.result.HasField("outcome"):
         stdout = result.result.outcome.completed.effects.stdout.inline.decode()
