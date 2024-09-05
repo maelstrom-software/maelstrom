6fa383a227cf7408641abf6f60381b277b68f20c
Philip Taron <philip.taron@gmail.com>
2024-09-05 12:54:54 -0700

    shell.nix: pin grpcio-tools to Python 3.11

--- shell.nix
+++ shell.nix
@@ -13,6 +13,7 @@
   rust-analyzer,
   stgit,
   maelstrom,
+  python311,
 }:
 
 craneLib.devShell {
@@ -33,6 +34,7 @@ craneLib.devShell {
     ripgrep
     rust-analyzer
     stgit
+    (python311.withPackages (ps: [ ps.grpcio-tools ]))
   ];
 
   env.CARGO_TARGET_WASM32_UNKNOWN_UNKNOWN_LINKER = "lld";
