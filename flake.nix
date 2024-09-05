5ee04785c649eafd36cd6e9503c4bac4ff7def0b
Remi Bernotavicius <remi@abort.cc>
2024-09-05 13:47:10 -0700

    Try updating crane nix packages

--- flake.nix
+++ flake.nix
@@ -6,7 +6,6 @@
     systems.url = "github:nix-systems/default";
 
     crane.url = "github:ipetkov/crane";
-    crane.inputs.nixpkgs.follows = "nixpkgs";
 
     flake-utils.url = "github:numtide/flake-utils";
     flake-utils.inputs.systems.follows = "systems";
