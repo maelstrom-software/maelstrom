721ef3a489bc8cb18f11cb559929d9708f18afa7
Remi Bernotavicius <remi@abort.cc>
2024-09-08 17:03:49 -0700

    Fix run-tests-on-maelstrom to actually fail if cargo-maelstrom does

--- scripts/run-tests-on-maelstrom-inner.sh
+++ scripts/run-tests-on-maelstrom-inner.sh
@@ -1,14 +1,11 @@
 #!/usr/bin/env bash
 
-set -x
+set -ex
 
 cargo build --release
-STATUS=$?
-if [ $STATUS -ne 0 ]; then
-	exit $STATUS
-fi
-
 py/protobuf_compile.sh
+
+set +e
 
 TEMPFILE=$(mktemp --tmpdir run-tests-on-maelstrom-broker-stderr.XXXXXX)
 cargo run --release --bin maelstrom-broker 2> >(tee "$TEMPFILE" >&2) &
@@ -19,15 +16,23 @@ PORT=$( \
 	| sed -Ee 's/^.*\baddr: [^,]*:([0-9]+),.*$/\1/' \
 )
 cargo run --release --bin maelstrom-worker -- --broker=localhost:$PORT &
-cargo run --release --bin cargo-maelstrom -- \
-    --broker=localhost:$PORT --profile=release
-cargo run --release --bin maelstrom-pytest -- \
-    --broker=localhost:$PORT
+
+cargo run --release --bin cargo-maelstrom -- --broker=localhost:$PORT --profile=release
 CARGO_MAELSTROM_STATUS=$?
+
+cargo run --release --bin maelstrom-pytest -- --broker=localhost:$PORT
+MAELSTROM_PYTEST_STATUS=$?
+
 kill -9 $BROKER_PID
 rm "$TEMPFILE"
+set -e
+
 if [ $CARGO_MAELSTROM_STATUS != 0 ]; then
 	exit $CARGO_MAELSTROM_STATUS
+fi
+
+if [ $MAELSTROM_PYTEST_STATUS != 0 ]; then
+	exit $MAELSTROM_PYTEST_STATUS
 fi
 
 cargo test --release --doc
