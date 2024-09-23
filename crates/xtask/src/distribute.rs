b29bc091c6bc194af6e92753a8e6edca82aca13b
Remi Bernotavicius <remi@abort.cc>
2024-09-23 13:48:03 -0700

    Update xtask distribute to be more general when binary patching
    fix: #419

--- crates/xtask/src/distribute.rs
+++ crates/xtask/src/distribute.rs
@@ -4,10 +4,11 @@ use elf::endian::AnyEndian;
 use elf::parse::ParseError;
 use elf::string_table::StringTable;
 use elf::ElfBytes;
+use std::collections::BTreeSet;
 use std::io::{Seek as _, Write as _};
-use std::mem;
 use std::path::{Path, PathBuf};
 use std::process::Command;
+use std::{fmt, mem};
 use zerocopy::{AsBytes, FromBytes, FromZeroes};
 
 #[derive(Clone, Debug, Default, PartialEq, FromZeroes, FromBytes, AsBytes)]
@@ -40,6 +41,23 @@ impl Vernaux {
 struct VerneedEntry {
     need: Verneed,
     aux: Vec<Vernaux>,
+}
+
+#[derive(Clone, Debug, FromZeroes, FromBytes, AsBytes)]
+#[repr(C)]
+struct Symbol {
+    name: u32,
+    info: u8,
+    other: u8,
+    shndx: u16,
+    value: u64,
+    size: u64,
+}
+
+impl Symbol {
+    fn name<'data>(&self, strtab: &'data StringTable) -> Result<&'data str, ParseError> {
+        strtab.get(self.name as usize)
+    }
 }
 
 fn decode_version_entries(mut data: &[u8]) -> Result<Vec<VerneedEntry>> {
@@ -193,7 +211,46 @@ fn encode_decode_version_entries() {
     );
 }
 
-fn remove_glibc_version_from_version_r(path: &Path, version: &str) -> Result<()> {
+struct GlibCVersion(versions::Versioning);
+
+impl GlibCVersion {
+    fn parse(s: &str) -> Option<Self> {
+        let mut iter = s.split('_');
+        if iter.next()? != "GLIBC" {
+            return None;
+        }
+        let version = versions::Versioning::new(iter.next()?)?;
+        if iter.next().is_some() {
+            return None;
+        }
+
+        Some(Self(version))
+    }
+
+    fn matches(&self, req: &versions::Requirement) -> bool {
+        req.matches(&self.0)
+    }
+}
+
+#[test]
+fn glibc_parsing() {
+    let v = GlibCVersion::parse("GLIBC_2.38").unwrap();
+    assert!(v.matches(&versions::Requirement::new(">=2.38").unwrap()));
+    assert!(!v.matches(&versions::Requirement::new("<2.25").unwrap()));
+
+    assert!(GlibCVersion::parse("").is_none());
+    assert!(GlibCVersion::parse("FOOBAR_2.38").is_none());
+    assert!(GlibCVersion::parse("GLIBC").is_none());
+    assert!(GlibCVersion::parse("GLIBC_").is_none());
+    assert!(GlibCVersion::parse("GLIBC_3...2").is_none());
+    assert!(GlibCVersion::parse("GLIBC_2.38_foo").is_none());
+}
+
+fn remove_glibc_versions_from_version_r(
+    path: &Path,
+    to_remove: versions::Requirement,
+    report: &mut BinaryPatchReport,
+) -> Result<Vec<u16>> {
     let file_data = std::fs::read(path)?;
     let slice = file_data.as_slice();
     let file = ElfBytes::<AnyEndian>::minimal_parse(slice)?;
@@ -211,11 +268,18 @@ fn remove_glibc_version_from_version_r(path: &Path, version: &str) -> Result<()>
     let mut entries = decode_version_entries(data)?;
 
     // Remove the version entry we are interested in
+    let mut removed = vec![];
     for entry in &mut entries {
         for aux in mem::take(&mut entry.aux) {
-            if aux.name(&strtab)? != version {
-                entry.aux.push(aux);
+            let name = aux.name(&strtab)?;
+            if let Some(v) = GlibCVersion::parse(name) {
+                if v.matches(&to_remove) {
+                    removed.push(aux.other);
+                    report.glibc_versions_removed.insert(name.to_owned());
+                    continue;
+                }
             }
+            entry.aux.push(aux);
         }
     }
 
@@ -227,11 +291,58 @@ fn remove_glibc_version_from_version_r(path: &Path, version: &str) -> Result<()>
     encoded.resize(gnu_version_header.sh_size as usize, 0);
 
     // Rewrite that section of the file
-    let mut file = std::fs::OpenOptions::new().write(true).open(path).unwrap();
-    file.seek(std::io::SeekFrom::Start(gnu_version_header.sh_offset))
-        .unwrap();
-    file.write_all(&encoded).unwrap();
+    let mut file = std::fs::OpenOptions::new().write(true).open(path)?;
+    file.seek(std::io::SeekFrom::Start(gnu_version_header.sh_offset))?;
+    file.write_all(&encoded)?;
 
+    Ok(removed)
+}
+
+fn remove_symbol_versions(
+    path: &Path,
+    versions_to_remove: Vec<u16>,
+    report: &mut BinaryPatchReport,
+) -> Result<()> {
+    let file_data = std::fs::read(path)?;
+    let slice = file_data.as_slice();
+    let file = ElfBytes::<AnyEndian>::minimal_parse(slice)?;
+
+    let dynstr = file
+        .section_header_by_name(".dynstr")?
+        .ok_or_else(|| anyhow!(".dynstr section not found"))?;
+    let strtab = file.section_data_as_strtab(&dynstr)?;
+
+    let dynamic_symbols = file.section_header_by_name(".dynsym").unwrap().unwrap();
+    let symbol_versions = file
+        .section_header_by_name(".gnu.version")
+        .unwrap()
+        .unwrap();
+    let (mut data, _) = file.section_data(&dynamic_symbols).unwrap();
+    let (symbol_version_data, _) = file.section_data(&symbol_versions).unwrap();
+
+    let mut file = std::fs::OpenOptions::new().write(true).open(path)?;
+    let mut symbol_index = 0;
+    while !data.is_empty() {
+        let symbol_data = &data[..mem::size_of::<Symbol>()];
+        let symbol = Symbol::ref_from(symbol_data).unwrap();
+        let version_index = u16::from_le_bytes([
+            symbol_version_data[symbol_index * 2],
+            symbol_version_data[symbol_index * 2 + 1],
+        ]) & 0x7fff; // highest bit is a flag
+
+        if versions_to_remove.contains(&version_index) {
+            let offset = symbol_versions.sh_offset + (symbol_index * 2) as u64;
+            file.seek(std::io::SeekFrom::Start(offset))?;
+            // 1 means a global symbol
+            file.write_all(&1u16.to_le_bytes())?;
+            report
+                .symbols_made_global
+                .insert(symbol.name(&strtab)?.to_owned());
+        }
+
+        data = &data[mem::size_of::<Symbol>()..];
+        symbol_index += 1;
+    }
     Ok(())
 }
 
@@ -246,18 +357,41 @@ fn patchelf(args: &[&str], path: impl AsRef<Path>) -> Result<String> {
     Ok(String::from_utf8(output.stdout).unwrap())
 }
 
-fn patch_binary(path: &Path) -> Result<()> {
-    // I'm not sure the best way to get this value, here I am copying it from the system ls binary
-    let interpreter = PathBuf::from(patchelf(&["--print-interpreter"], "/bin/ls")?.trim());
-    let interpreter_str = interpreter.to_str().unwrap();
+#[derive(Default)]
+struct BinaryPatchReport {
+    glibc_versions_removed: BTreeSet<String>,
+    symbols_made_global: BTreeSet<String>,
+}
 
-    patchelf(&["--set-interpreter", interpreter_str], path)?;
+impl fmt::Display for BinaryPatchReport {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        writeln!(
+            f,
+            "glibc versions removed: {:?}",
+            &self.glibc_versions_removed
+        )?;
+        write!(f, "symbols made global: {:?}", &self.symbols_made_global)?;
+        Ok(())
+    }
+}
+
+fn patch_binary(path: &Path, report: &mut BinaryPatchReport) -> Result<()> {
+    // I'm not sure the best way to get this value, here I am copying it from the system ls binary
+    let interpreter_str = patchelf(&["--print-interpreter"], "/bin/ls")?
+        .trim()
+        .to_owned();
+
+    // Remove the nix specific stuff
+    patchelf(&["--set-interpreter", &interpreter_str], path)?;
     patchelf(&["--remove-rpath"], path)?;
-    patchelf(&["--clear-symbol-version", "fmod"], path)?;
-    patchelf(&["--clear-symbol-version", "pidfd_spawnp"], path)?;
-    patchelf(&["--clear-symbol-version", "pidfd_getpid"], path)?;
-    remove_glibc_version_from_version_r(path, "GLIBC_2.38")?;
-    remove_glibc_version_from_version_r(path, "GLIBC_2.39")?;
+
+    // The highest version of glibc for Ubuntu 22.04 is 2.35
+    let req = versions::Requirement::new(">2.35").unwrap();
+
+    // Remove any requirement on high versions of glibc
+    let removed_versions = remove_glibc_versions_from_version_r(path, req, report)?;
+    remove_symbol_versions(path, removed_versions, report)?;
+
     Ok(())
 }
 
@@ -312,10 +446,11 @@ fn package_artifacts(
     binaries: &[PathBuf],
 ) -> Result<Vec<PathBuf>> {
     let mut packaged = vec![];
+    let mut report = BinaryPatchReport::default();
     for binary_path in binaries {
         let new_binary = temp_dir.path().join(binary_path.file_name().unwrap());
         std::fs::copy(binary_path, &new_binary)?;
-        patch_binary(&new_binary)?;
+        patch_binary(&new_binary, &mut report)?;
         let tar_gz_path = temp_dir.path().join(format!(
             "{}-{target_triple}.tgz",
             new_binary.file_name().unwrap().to_str().unwrap()
@@ -323,6 +458,7 @@ fn package_artifacts(
         tar_gz(&new_binary, &tar_gz_path)?;
         packaged.push(tar_gz_path)
     }
+    println!("{report}");
     Ok(packaged)
 }
 
