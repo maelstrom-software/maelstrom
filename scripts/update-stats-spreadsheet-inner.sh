dc30861229620f5e0615f6755fa15d790f19ff0b
Remi Bernotavicius <remi@abort.cc>
2024-10-01 11:57:13 -0700

    Update update-stats-spreadsheet.sh

--- scripts/update-stats-spreadsheet-inner.sh
+++ scripts/update-stats-spreadsheet-inner.sh
@@ -4,10 +4,10 @@ set -ex
 
 if [ -z "$1" ] || [ -z "$2" ]
 then
-    echo "args: <gsheets-service-account-json-path> <gh-token>"
+    echo "args: <gsheets-service-account-json> <gh-token>"
     exit 1
 fi
 
-export SERVICE_ACCOUNT_JSON=$(cat "$1")
+export SERVICE_ACCOUNT_JSON=$1
 echo "$2" | gh auth login --with-token
 cargo xtask pull-stats
