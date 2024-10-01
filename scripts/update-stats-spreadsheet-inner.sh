cf5a96b81bd5723b0ef4444b346dfb667b7b8a92
Remi Bernotavicius <remi@abort.cc>
2024-10-01 12:09:37 -0700

    Try to get update-stats-spreadsheet.yml working

--- scripts/update-stats-spreadsheet-inner.sh
+++ scripts/update-stats-spreadsheet-inner.sh
@@ -1,13 +1,3 @@
 #!/bin/bash
 
-set -ex
-
-if [ -z "$1" ] || [ -z "$2" ]
-then
-    echo "args: <gsheets-service-account-json> <gh-token>"
-    exit 1
-fi
-
-export SERVICE_ACCOUNT_JSON=$1
-echo "$2" | gh auth login --with-token
-cargo xtask pull-stats
+exec cargo xtask pull-stats
