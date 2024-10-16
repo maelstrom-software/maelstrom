337d23840238fc6ad86fffb2255ed48c5b2f36d4
Neal Fachan <neal@fachan.com>
2024-10-16 16:12:58 -0700

    Re-order functions in maelstrom-worker/src/lib.rs.

--- crates/maelstrom-worker/src/lib.rs
+++ crates/maelstrom-worker/src/lib.rs
@@ -38,161 +38,8 @@ pub struct WorkerCacheDir;
 pub const MAX_IN_FLIGHT_LAYERS_BUILDS: usize = 10;
 pub const MAX_ARTIFACT_FETCHES: usize = 10;
 
-/// Returns error from shutdown message, or delivers message to dispatcher.
-fn handle_dispatcher_message(msg: Message<StdFs>, dispatcher: &mut Dispatcher) -> Result<()> {
-    if let Message::Shutdown(error) = msg {
-        return Err(error);
-    }
-
-    dispatcher.receive_message(msg);
-    Ok(())
-}
-
-async fn handle_incoming_messages(
-    log: Logger,
-    mut dispatcher_receiver: DispatcherReceiver,
-    mut broker_socket_incoming_recevier: BrokerSocketIncomingReceiver,
-    mut dispatcher: Dispatcher,
-) {
-    // Multiplex messages from broker and others sources
-    let err = loop {
-        let res = tokio::select! {
-            msg = dispatcher_receiver.recv() => {
-                handle_dispatcher_message(msg.expect("missing shutdown"), &mut dispatcher)
-            },
-            msg = broker_socket_incoming_recevier.recv() => {
-                let Some(msg) = msg else { continue };
-                handle_dispatcher_message(Message::Broker(msg), &mut dispatcher)
-            },
-        };
-        if let Err(err) = res {
-            break err;
-        }
-    };
-
-    error!(log, "shutting down due to {err}");
-
-    // This should close the connection with the broker, and canceling running jobs.
-    info!(
-        log,
-        "canceling {} running jobs",
-        dispatcher.num_jobs_executing()
-    );
-    dispatcher.receive_message(Message::Shutdown(err));
-    drop(broker_socket_incoming_recevier);
-
-    // Wait for the running jobs to finish.
-    while dispatcher.num_jobs_executing() > 0 {
-        let msg = dispatcher_receiver.recv().await.expect("missing shutdown");
-        let _ = handle_dispatcher_message(msg, &mut dispatcher);
-    }
-}
-
-async fn dispatcher_main(
-    config: Config,
-    dispatcher_receiver: DispatcherReceiver,
-    dispatcher_sender: DispatcherSender,
-    broker_socket_outgoing_sender: BrokerSocketOutgoingSender,
-    broker_socket_incoming_receiver: BrokerSocketIncomingReceiver,
-    log: Logger,
-) {
-    let mount_dir = config.cache_root.join::<MountDir>("mount");
-    let tmpfs_dir = config.cache_root.join::<TmpfsDir>("upper");
-    let cache_root = config.cache_root.join::<CacheDir>("artifacts");
-    let blob_dir = cache_root.join::<BlobDir>("sha256/blob");
-
-    let broker_sender = BrokerSender::new(broker_socket_outgoing_sender);
-    let (cache, temp_file_factory) =
-        match Cache::new(StdFs, cache_root, config.cache_size, log.clone()) {
-            Err(err) => {
-                error!(log, "could not start cache"; "err" => ?err);
-                return;
-            }
-            Ok((cache, temp_file_factory)) => (cache, temp_file_factory),
-        };
-    let artifact_fetcher = ArtifactFetcher::new(
-        MAX_ARTIFACT_FETCHES,
-        dispatcher_sender.clone(),
-        config.broker,
-        log.clone(),
-        temp_file_factory.clone(),
-    );
-    match DispatcherAdapter::new(
-        dispatcher_sender,
-        config.inline_limit,
-        log.clone(),
-        mount_dir,
-        tmpfs_dir,
-        blob_dir,
-        temp_file_factory,
-    ) {
-        Err(err) => {
-            error!(log, "could not start executor"; "err" => ?err);
-        }
-        Ok(adapter) => {
-            let dispatcher = Dispatcher::new(
-                adapter,
-                artifact_fetcher,
-                broker_sender,
-                cache,
-                config.slots,
-            );
-            handle_incoming_messages(
-                log,
-                dispatcher_receiver,
-                broker_socket_incoming_receiver,
-                dispatcher,
-            )
-            .await;
-        }
-    }
-}
-
-async fn shutdown_on_error(
-    fut: impl Future<Output = Result<()>>,
-    dispatcher_sender: DispatcherSender,
-) {
-    if let Err(error) = fut.await {
-        let _ = dispatcher_sender.send(Message::Shutdown(error));
-    }
-}
-
-async fn wait_for_signal(log: Logger) -> Result<()> {
-    let signal = signal::wait_for_signal(log).await;
-    Err(anyhow!("signal {signal}"))
-}
-
-/// For the number of slots, what is the maximum number of files we will open. This attempts to
-/// come up with a number by doing some math, but nothing is guaranteeing the result.
-fn open_file_max(slots: Slots) -> u64 {
-    let existing_open_files: u64 = 3 /* stdout, stdin, stderr */;
-    let per_slot_estimate: u64 = 6 /* unix socket, FUSE connection, (stdout, stderr) * 2 */ +
-        maelstrom_fuse::MAX_INFLIGHT as u64 /* each FUSE request opens a file */;
-    existing_open_files
-        + (maelstrom_layer_fs::READER_CACHE_SIZE * 2) // 1 for socket, 1 for the file
-        + MAX_ARTIFACT_FETCHES as u64
-        + per_slot_estimate * u16::from(slots) as u64
-        + (MAX_IN_FLIGHT_LAYERS_BUILDS * maelstrom_layer_fs::LAYER_BUILDING_FILE_MAX) as u64
-}
-
-fn round_to_multiple(n: u64, k: u64) -> u64 {
-    if n % k == 0 {
-        n
-    } else {
-        n + (k - (n % k))
-    }
-}
-
-/// Check if the open file limit is high enough to fit our estimate of how many files we need.
-pub fn check_open_file_limit(log: &Logger, slots: Slots, extra: u64) -> Result<()> {
-    let limit = linux::getrlimit(linux::RlimitResource::NoFile)?;
-    let estimate = open_file_max(slots) + extra;
-    debug!(log, "checking open file limit"; "limit" => ?limit.current, "estimate" => estimate);
-    if limit.current < estimate {
-        let estimate = round_to_multiple(estimate, 1024);
-        bail!("Open file limit is too low. Increase limit by running `ulimit -n {estimate}`");
-    }
-    Ok(())
+pub fn main(config: Config, log: Logger) -> Result<()> {
+    main_inner(config, log)
 }
 
 /// The main function for the worker. This should be called on a task of its own. It will return
@@ -275,6 +122,159 @@ pub async fn main_inner(config: Config, log: Logger) -> Result<()> {
     Ok(())
 }
 
-pub fn main(config: Config, log: Logger) -> Result<()> {
-    main_inner(config, log)
+/// Check if the open file limit is high enough to fit our estimate of how many files we need.
+pub fn check_open_file_limit(log: &Logger, slots: Slots, extra: u64) -> Result<()> {
+    let limit = linux::getrlimit(linux::RlimitResource::NoFile)?;
+    let estimate = open_file_max(slots) + extra;
+    debug!(log, "checking open file limit"; "limit" => ?limit.current, "estimate" => estimate);
+    if limit.current < estimate {
+        let estimate = round_to_multiple(estimate, 1024);
+        bail!("Open file limit is too low. Increase limit by running `ulimit -n {estimate}`");
+    }
+    Ok(())
+}
+
+/// For the number of slots, what is the maximum number of files we will open. This attempts to
+/// come up with a number by doing some math, but nothing is guaranteeing the result.
+fn open_file_max(slots: Slots) -> u64 {
+    let existing_open_files: u64 = 3 /* stdout, stdin, stderr */;
+    let per_slot_estimate: u64 = 6 /* unix socket, FUSE connection, (stdout, stderr) * 2 */ +
+        maelstrom_fuse::MAX_INFLIGHT as u64 /* each FUSE request opens a file */;
+    existing_open_files
+        + (maelstrom_layer_fs::READER_CACHE_SIZE * 2) // 1 for socket, 1 for the file
+        + MAX_ARTIFACT_FETCHES as u64
+        + per_slot_estimate * u16::from(slots) as u64
+        + (MAX_IN_FLIGHT_LAYERS_BUILDS * maelstrom_layer_fs::LAYER_BUILDING_FILE_MAX) as u64
+}
+
+fn round_to_multiple(n: u64, k: u64) -> u64 {
+    if n % k == 0 {
+        n
+    } else {
+        n + (k - (n % k))
+    }
+}
+
+async fn shutdown_on_error(
+    fut: impl Future<Output = Result<()>>,
+    dispatcher_sender: DispatcherSender,
+) {
+    if let Err(error) = fut.await {
+        let _ = dispatcher_sender.send(Message::Shutdown(error));
+    }
+}
+
+async fn wait_for_signal(log: Logger) -> Result<()> {
+    let signal = signal::wait_for_signal(log).await;
+    Err(anyhow!("signal {signal}"))
+}
+
+async fn dispatcher_main(
+    config: Config,
+    dispatcher_receiver: DispatcherReceiver,
+    dispatcher_sender: DispatcherSender,
+    broker_socket_outgoing_sender: BrokerSocketOutgoingSender,
+    broker_socket_incoming_receiver: BrokerSocketIncomingReceiver,
+    log: Logger,
+) {
+    let mount_dir = config.cache_root.join::<MountDir>("mount");
+    let tmpfs_dir = config.cache_root.join::<TmpfsDir>("upper");
+    let cache_root = config.cache_root.join::<CacheDir>("artifacts");
+    let blob_dir = cache_root.join::<BlobDir>("sha256/blob");
+
+    let broker_sender = BrokerSender::new(broker_socket_outgoing_sender);
+    let (cache, temp_file_factory) =
+        match Cache::new(StdFs, cache_root, config.cache_size, log.clone()) {
+            Err(err) => {
+                error!(log, "could not start cache"; "err" => ?err);
+                return;
+            }
+            Ok((cache, temp_file_factory)) => (cache, temp_file_factory),
+        };
+    let artifact_fetcher = ArtifactFetcher::new(
+        MAX_ARTIFACT_FETCHES,
+        dispatcher_sender.clone(),
+        config.broker,
+        log.clone(),
+        temp_file_factory.clone(),
+    );
+    match DispatcherAdapter::new(
+        dispatcher_sender,
+        config.inline_limit,
+        log.clone(),
+        mount_dir,
+        tmpfs_dir,
+        blob_dir,
+        temp_file_factory,
+    ) {
+        Err(err) => {
+            error!(log, "could not start executor"; "err" => ?err);
+        }
+        Ok(adapter) => {
+            let dispatcher = Dispatcher::new(
+                adapter,
+                artifact_fetcher,
+                broker_sender,
+                cache,
+                config.slots,
+            );
+            handle_incoming_messages(
+                log,
+                dispatcher_receiver,
+                broker_socket_incoming_receiver,
+                dispatcher,
+            )
+            .await;
+        }
+    }
+}
+
+async fn handle_incoming_messages(
+    log: Logger,
+    mut dispatcher_receiver: DispatcherReceiver,
+    mut broker_socket_incoming_recevier: BrokerSocketIncomingReceiver,
+    mut dispatcher: Dispatcher,
+) {
+    // Multiplex messages from broker and others sources
+    let err = loop {
+        let res = tokio::select! {
+            msg = dispatcher_receiver.recv() => {
+                handle_dispatcher_message(msg.expect("missing shutdown"), &mut dispatcher)
+            },
+            msg = broker_socket_incoming_recevier.recv() => {
+                let Some(msg) = msg else { continue };
+                handle_dispatcher_message(Message::Broker(msg), &mut dispatcher)
+            },
+        };
+        if let Err(err) = res {
+            break err;
+        }
+    };
+
+    error!(log, "shutting down due to {err}");
+
+    // This should close the connection with the broker, and canceling running jobs.
+    info!(
+        log,
+        "canceling {} running jobs",
+        dispatcher.num_jobs_executing()
+    );
+    dispatcher.receive_message(Message::Shutdown(err));
+    drop(broker_socket_incoming_recevier);
+
+    // Wait for the running jobs to finish.
+    while dispatcher.num_jobs_executing() > 0 {
+        let msg = dispatcher_receiver.recv().await.expect("missing shutdown");
+        let _ = handle_dispatcher_message(msg, &mut dispatcher);
+    }
+}
+
+/// Returns error from shutdown message, or delivers message to dispatcher.
+fn handle_dispatcher_message(msg: Message<StdFs>, dispatcher: &mut Dispatcher) -> Result<()> {
+    if let Message::Shutdown(error) = msg {
+        return Err(error);
+    }
+
+    dispatcher.receive_message(msg);
+    Ok(())
 }
