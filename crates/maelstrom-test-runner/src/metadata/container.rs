0bdd66ade44e097eb794a4b25fa54ce40a2fee3f
Neal Fachan <neal@fachan.com>
2024-11-14 13:33:14 -0800

    Completed testing of deserialize for TestContainer.

--- crates/maelstrom-test-runner/src/metadata/container.rs
+++ crates/maelstrom-test-runner/src/metadata/container.rs
@@ -160,35 +160,32 @@ impl<'de> de::Deserialize<'de> for TestContainer {
 mod tests {
     use super::*;
     use anyhow::Error;
-    use indoc::indoc;
     use maelstrom_base::{enum_set, JobDeviceForTomlAndJson};
     use maelstrom_client::spec::SymlinkSpec;
     use maelstrom_test::{
         glob_layer, non_root_utf8_path_buf, paths_layer, shared_library_dependencies_layer, string,
         tar_layer, utf8_path_buf,
     };
-    use toml::de::Error as TomlError;
-
-    fn parse_test_container(file: &str) -> Result<TestContainer> {
-        toml::from_str(file).map_err(Error::new)
-    }
+    use maplit::btreemap;
 
     #[track_caller]
-    fn container_error_test(toml: &str, expected: &str) {
-        let err = parse_test_container(toml).unwrap_err();
-        let err = err.downcast_ref::<TomlError>().unwrap();
-        let message = err.message();
-        assert!(message.starts_with(expected), "message: {message}");
+    fn container_parse_error_test(toml: &str, expected: &str) {
+        let err = toml::from_str::<TestContainer>(toml).unwrap_err();
+        let actual = err.message();
+        assert!(
+            actual.starts_with(expected),
+            "expected: {expected}; actual: {actual}"
+        );
     }
 
     #[track_caller]
     fn container_parse_test(toml: &str, expected: TestContainer) {
-        assert_eq!(parse_test_container(toml).unwrap(), expected);
+        assert_eq!(toml::from_str::<TestContainer>(toml).unwrap(), expected);
     }
 
     #[test]
     fn unknown_field() {
-        container_error_test(
+        container_parse_error_test(
             r#"
             unknown = "foo"
             "#,
@@ -198,7 +195,7 @@ mod tests {
 
     #[test]
     fn duplicate_field() {
-        container_error_test(
+        container_parse_error_test(
             r#"
             user = 100
             user = 100
@@ -227,16 +224,27 @@ mod tests {
     }
 
     #[test]
-    fn added_mounts_after_mounts() {
+    fn mounts() {
         container_parse_test(
-            indoc! {r#"
-                mounts = [ { type = "proc", mount_point = "/proc" } ]
-                added_mounts = [ { type = "tmp", mount_point = "/tmp" } ]
-            "#},
+            r#"
+            mounts = [ { type = "proc", mount_point = "/proc" } ]
+            "#,
             TestContainer {
                 mounts: Some(vec![JobMountForTomlAndJson::Proc {
                     mount_point: non_root_utf8_path_buf!("/proc"),
                 }]),
+                ..Default::default()
+            },
+        );
+    }
+
+    #[test]
+    fn added_mounts() {
+        container_parse_test(
+            r#"
+            added_mounts = [ { type = "tmp", mount_point = "/tmp" } ]
+            "#,
+            TestContainer {
                 added_mounts: vec![JobMountForTomlAndJson::Tmp {
                     mount_point: non_root_utf8_path_buf!("/tmp"),
                 }],
@@ -246,19 +254,472 @@ mod tests {
     }
 
     #[test]
-    fn mounts_after_added_mounts() {
+    fn working_directory() {
         container_parse_test(
-            indoc! {r#"
-                added_mounts = [ { type = "tmp", mount_point = "/tmp" } ]
-                mounts = [ { type = "proc", mount_point = "/proc" } ]
-            "#},
+            r#"
+            working_directory = "/foo"
+            "#,
             TestContainer {
-                mounts: Some(vec![JobMountForTomlAndJson::Proc {
-                    mount_point: non_root_utf8_path_buf!("/proc"),
-                }]),
-                added_mounts: vec![JobMountForTomlAndJson::Tmp {
-                    mount_point: non_root_utf8_path_buf!("/tmp"),
-                }],
+                working_directory: Some(PossiblyImage::Explicit("/foo".into())),
+                ..Default::default()
+            },
+        );
+    }
+
+    #[test]
+    fn image_with_working_directory() {
+        container_parse_test(
+            r#"
+            image = { name = "rust", use = ["layers", "working_directory"] }
+            "#,
+            TestContainer {
+                image: Some(string!("rust")),
+                working_directory: Some(PossiblyImage::Image),
+                layers: Some(PossiblyImage::Image),
+                ..Default::default()
+            },
+        );
+    }
+
+    #[test]
+    fn working_directory_before_image_without_working_directory() {
+        container_parse_test(
+            r#"
+            working_directory = "/foo"
+            image = "rust"
+            "#,
+            TestContainer {
+                image: Some(string!("rust")),
+                working_directory: Some(PossiblyImage::Explicit("/foo".into())),
+                layers: Some(PossiblyImage::Image),
+                environment: Some(PossiblyImage::Image),
+                ..Default::default()
+            },
+        );
+    }
+
+    #[test]
+    fn working_directory_after_image_without_working_directory() {
+        container_parse_test(
+            r#"
+            image = "rust"
+            working_directory = "/foo"
+            "#,
+            TestContainer {
+                image: Some(string!("rust")),
+                working_directory: Some(PossiblyImage::Explicit("/foo".into())),
+                layers: Some(PossiblyImage::Image),
+                environment: Some(PossiblyImage::Image),
+                ..Default::default()
+            },
+        );
+    }
+
+    #[test]
+    fn working_directory_before_image_with_working_directory() {
+        container_parse_error_test(
+            r#"
+            working_directory = "/foo"
+            image = { name = "rust", use = ["layers", "working_directory"] }
+            "#,
+            "field `image` cannot use `working_directory` if field `working_directory` is also set",
+        );
+    }
+
+    #[test]
+    fn working_directory_after_image_with_working_directory() {
+        container_parse_error_test(
+            r#"
+            image = { name = "rust", use = ["layers", "working_directory"] }
+            working_directory = "/foo"
+            "#,
+            "field `image` cannot use `working_directory` if field `working_directory` is also set",
+        );
+    }
+
+    #[test]
+    fn layers() {
+        container_parse_test(
+            r#"
+            layers = [ { tar = "foo.tar" } ]
+            "#,
+            TestContainer {
+                layers: Some(PossiblyImage::Explicit(vec![tar_layer!("foo.tar")])),
+                ..Default::default()
+            },
+        );
+    }
+
+    #[test]
+    fn added_layers() {
+        container_parse_test(
+            r#"
+            added_layers = [ { tar = "bar.tar" } ]
+            "#,
+            TestContainer {
+                added_layers: vec![tar_layer!("bar.tar")],
+                ..Default::default()
+            },
+        );
+    }
+
+    #[test]
+    fn layers_before_added_layers() {
+        container_parse_test(
+            r#"
+            layers = [ { tar = "foo.tar" } ]
+            added_layers = [ { tar = "bar.tar" } ]
+            "#,
+            TestContainer {
+                layers: Some(PossiblyImage::Explicit(vec![tar_layer!("foo.tar")])),
+                added_layers: vec![tar_layer!("bar.tar")],
+                ..Default::default()
+            },
+        );
+    }
+
+    #[test]
+    fn layers_after_added_layers() {
+        container_parse_test(
+            r#"
+            added_layers = [ { tar = "bar.tar" } ]
+            layers = [ { tar = "foo.tar" } ]
+            "#,
+            TestContainer {
+                layers: Some(PossiblyImage::Explicit(vec![tar_layer!("foo.tar")])),
+                added_layers: vec![tar_layer!("bar.tar")],
+                ..Default::default()
+            },
+        );
+    }
+
+    #[test]
+    fn image_with_layers() {
+        container_parse_test(
+            r#"
+            image = { name = "rust", use = ["layers"] }
+            "#,
+            TestContainer {
+                image: Some(string!("rust")),
+                layers: Some(PossiblyImage::Image),
+                ..Default::default()
+            },
+        );
+    }
+
+    #[test]
+    fn image_with_layers_before_added_layers() {
+        container_parse_test(
+            r#"
+            image = { name = "rust", use = ["layers"] }
+            added_layers = [ { tar = "bar.tar" } ]
+            "#,
+            TestContainer {
+                image: Some(string!("rust")),
+                layers: Some(PossiblyImage::Image),
+                added_layers: vec![tar_layer!("bar.tar")],
+                ..Default::default()
+            },
+        );
+    }
+
+    #[test]
+    fn image_with_layers_after_added_layers() {
+        container_parse_test(
+            r#"
+            added_layers = [ { tar = "bar.tar" } ]
+            image = { name = "rust", use = ["layers"] }
+            "#,
+            TestContainer {
+                image: Some(string!("rust")),
+                layers: Some(PossiblyImage::Image),
+                added_layers: vec![tar_layer!("bar.tar")],
+                ..Default::default()
+            },
+        );
+    }
+
+    #[test]
+    fn image_without_layers_before_added_layers() {
+        container_parse_test(
+            r#"
+            image = { name = "rust", use = ["environment"] }
+            added_layers = [ { tar = "bar.tar" } ]
+            "#,
+            TestContainer {
+                image: Some(string!("rust")),
+                environment: Some(PossiblyImage::Image),
+                added_layers: vec![tar_layer!("bar.tar")],
+                ..Default::default()
+            },
+        );
+    }
+
+    #[test]
+    fn image_without_layers_after_added_layers() {
+        container_parse_test(
+            r#"
+            added_layers = [ { tar = "bar.tar" } ]
+            image = { name = "rust", use = ["environment"] }
+            "#,
+            TestContainer {
+                image: Some(string!("rust")),
+                environment: Some(PossiblyImage::Image),
+                added_layers: vec![tar_layer!("bar.tar")],
+                ..Default::default()
+            },
+        );
+    }
+
+    #[test]
+    fn image_with_layers_before_layers() {
+        container_parse_error_test(
+            r#"
+            image = { name = "rust", use = ["layers"] }
+            layers = [ { tar = "foo.tar" } ]
+            "#,
+            "field `image` cannot use `layers` if field `layers` is also set",
+        );
+    }
+
+    #[test]
+    fn image_with_layers_after_layers() {
+        container_parse_error_test(
+            r#"
+            layers = [ { tar = "foo.tar" } ]
+            image = { name = "rust", use = ["layers"] }
+            "#,
+            "field `image` cannot use `layers` if field `layers` is also set",
+        );
+    }
+
+    #[test]
+    fn image_without_layers_before_layers() {
+        container_parse_test(
+            r#"
+            image = { name = "rust", use = ["environment"] }
+            layers = [ { tar = "foo.tar" } ]
+            "#,
+            TestContainer {
+                image: Some(string!("rust")),
+                environment: Some(PossiblyImage::Image),
+                layers: Some(PossiblyImage::Explicit(vec![tar_layer!("foo.tar")])),
+                ..Default::default()
+            },
+        );
+    }
+
+    #[test]
+    fn image_without_layers_after_layers() {
+        container_parse_test(
+            r#"
+            layers = [ { tar = "foo.tar" } ]
+            image = { name = "rust", use = ["environment"] }
+            "#,
+            TestContainer {
+                image: Some(string!("rust")),
+                environment: Some(PossiblyImage::Image),
+                layers: Some(PossiblyImage::Explicit(vec![tar_layer!("foo.tar")])),
+                ..Default::default()
+            },
+        );
+    }
+
+    /*** environment ***/
+
+    #[test]
+    fn environment() {
+        container_parse_test(
+            r#"
+            environment = { FOO = "foo" }
+            "#,
+            TestContainer {
+                environment: Some(PossiblyImage::Explicit(
+                    btreemap! { string!("FOO") => string!("foo") },
+                )),
+                ..Default::default()
+            },
+        );
+    }
+
+    #[test]
+    fn added_environment() {
+        container_parse_test(
+            r#"
+            added_environment = { BAR = "bar" }
+            "#,
+            TestContainer {
+                added_environment: btreemap! { string!("BAR") => string!("bar") },
+                ..Default::default()
+            },
+        );
+    }
+
+    #[test]
+    fn environment_before_added_environment() {
+        container_parse_test(
+            r#"
+            environment = { FOO = "foo" }
+            added_environment = { BAR = "bar" }
+            "#,
+            TestContainer {
+                environment: Some(PossiblyImage::Explicit(
+                    btreemap! { string!("FOO") => string!("foo") },
+                )),
+                added_environment: btreemap! { string!("BAR") => string!("bar") },
+                ..Default::default()
+            },
+        );
+    }
+
+    #[test]
+    fn environment_after_added_environment() {
+        container_parse_test(
+            r#"
+            added_environment = { BAR = "bar" }
+            environment = { FOO = "foo" }
+            "#,
+            TestContainer {
+                environment: Some(PossiblyImage::Explicit(
+                    btreemap! { string!("FOO") => string!("foo") },
+                )),
+                added_environment: btreemap! { string!("BAR") => string!("bar") },
+                ..Default::default()
+            },
+        );
+    }
+
+    #[test]
+    fn image_with_environment() {
+        container_parse_test(
+            r#"
+            image = { name = "rust", use = ["environment"] }
+            "#,
+            TestContainer {
+                image: Some(string!("rust")),
+                environment: Some(PossiblyImage::Image),
+                ..Default::default()
+            },
+        );
+    }
+
+    #[test]
+    fn image_with_environment_before_added_environment() {
+        container_parse_test(
+            r#"
+            image = { name = "rust", use = ["environment"] }
+            added_environment = { BAR = "bar" }
+            "#,
+            TestContainer {
+                image: Some(string!("rust")),
+                environment: Some(PossiblyImage::Image),
+                added_environment: btreemap! { string!("BAR") => string!("bar") },
+                ..Default::default()
+            },
+        );
+    }
+
+    #[test]
+    fn image_with_environment_after_added_environment() {
+        container_parse_test(
+            r#"
+            added_environment = { BAR = "bar" }
+            image = { name = "rust", use = ["environment"] }
+            "#,
+            TestContainer {
+                image: Some(string!("rust")),
+                environment: Some(PossiblyImage::Image),
+                added_environment: btreemap! { string!("BAR") => string!("bar") },
+                ..Default::default()
+            },
+        );
+    }
+
+    #[test]
+    fn image_without_environment_before_added_environment() {
+        container_parse_test(
+            r#"
+            image = { name = "rust", use = ["layers"] }
+            added_environment = { BAR = "bar" }
+            "#,
+            TestContainer {
+                image: Some(string!("rust")),
+                layers: Some(PossiblyImage::Image),
+                added_environment: btreemap! { string!("BAR") => string!("bar") },
+                ..Default::default()
+            },
+        );
+    }
+
+    #[test]
+    fn image_without_environment_after_added_environment() {
+        container_parse_test(
+            r#"
+            added_environment = { BAR = "bar" }
+            image = { name = "rust", use = ["layers"] }
+            "#,
+            TestContainer {
+                image: Some(string!("rust")),
+                layers: Some(PossiblyImage::Image),
+                added_environment: btreemap! { string!("BAR") => string!("bar") },
+                ..Default::default()
+            },
+        );
+    }
+
+    #[test]
+    fn image_with_environment_before_environment() {
+        container_parse_error_test(
+            r#"
+            image = { name = "rust", use = ["environment"] }
+            environment = { FOO = "foo" }
+            "#,
+            "field `image` cannot use `environment` if field `environment` is also set",
+        );
+    }
+
+    #[test]
+    fn image_with_environment_after_environment() {
+        container_parse_error_test(
+            r#"
+            environment = { FOO = "foo" }
+            image = { name = "rust", use = ["environment"] }
+            "#,
+            "field `image` cannot use `environment` if field `environment` is also set",
+        );
+    }
+
+    #[test]
+    fn image_without_environment_before_environment() {
+        container_parse_test(
+            r#"
+            image = { name = "rust", use = ["layers"] }
+            environment = { FOO = "foo" }
+            "#,
+            TestContainer {
+                image: Some(string!("rust")),
+                layers: Some(PossiblyImage::Image),
+                environment: Some(PossiblyImage::Explicit(
+                    btreemap! { string!("FOO") => string!("foo") },
+                )),
+                ..Default::default()
+            },
+        );
+    }
+
+    #[test]
+    fn image_without_environment_after_environment() {
+        container_parse_test(
+            r#"
+            environment = { FOO = "foo" }
+            image = { name = "rust", use = ["layers"] }
+            "#,
+            TestContainer {
+                image: Some(string!("rust")),
+                layers: Some(PossiblyImage::Image),
+                environment: Some(PossiblyImage::Explicit(
+                    btreemap! { string!("FOO") => string!("foo") },
+                )),
                 ..Default::default()
             },
         );
