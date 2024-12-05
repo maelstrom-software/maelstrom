70efd34ffcd59490a70fd18834b9d762c1300775
Remi Bernotavicius <remi@abort.cc>
2024-12-04 18:01:17 -0800

    Abstract artifact_pusher::start_task to reuse some code.

--- crates/maelstrom-client-process/src/artifact_pusher.rs
+++ crates/maelstrom-client-process/src/artifact_pusher.rs
@@ -1,15 +1,21 @@
 mod tcp_upload;
 
+use anyhow::Result;
 use maelstrom_base::Sha256Digest;
-use std::path::PathBuf;
+use maelstrom_util::r#async::Pool;
+use std::future::Future;
+use std::{num::NonZeroU32, path::PathBuf, sync::Arc};
 use tokio::sync::mpsc::{self, UnboundedReceiver, UnboundedSender};
+use tokio::task::JoinSet;
 
 pub use tcp_upload::start_task;
+
+type SuccessCb = Box<dyn FnOnce() + Send + Sync>;
 
 pub struct Message {
     pub path: PathBuf,
     pub digest: Sha256Digest,
-    pub success_callback: Box<dyn FnOnce() + Send + Sync>,
+    pub success_callback: SuccessCb,
 }
 
 pub type Sender = UnboundedSender<Message>;
@@ -20,3 +26,53 @@ pub fn channel() -> (Sender, Receiver) {
 }
 
 pub const MAX_CLIENT_UPLOADS: usize = 10;
+
+fn start_task_inner<PoolItemT, RetFutT, PushOneArtifactFn>(
+    join_set: &mut JoinSet<Result<()>>,
+    mut receiver: Receiver,
+    push_one_artifact: PushOneArtifactFn,
+) where
+    PoolItemT: Send + 'static,
+    RetFutT: Future<Output = Result<(PoolItemT, ())>> + Send,
+    PushOneArtifactFn: FnMut(Option<PoolItemT>, PathBuf, Sha256Digest, SuccessCb) -> RetFutT
+        + Clone
+        + Send
+        + 'static,
+{
+    let pool = Arc::new(Pool::new(
+        NonZeroU32::try_from(u32::try_from(MAX_CLIENT_UPLOADS).unwrap()).unwrap(),
+    ));
+    join_set.spawn(async move {
+        // When this join_set gets destroyed, all outstanding artifact pusher tasks will be
+        // canceled. That will happen either when our sender is closed, or when our own task is
+        // canceled. In either case, it means the process is shutting down.
+        //
+        // We have to be careful not to let the join_set grow indefinitely. This is why we select!
+        // below. We always wait on the join_set's join_next. If it's an actual error, we propagate
+        // it, which will immediately cancel all the other outstanding tasks.
+        let mut join_set = JoinSet::new();
+        loop {
+            tokio::select! {
+                Some(res) = join_set.join_next() => {
+                    res.unwrap()?; // We don't expect JoinErrors.
+                },
+                res = receiver.recv() => {
+                    let Some(Message { path, digest, success_callback } ) = res else { break; };
+                    let pool = pool.clone();
+                    let mut push_one_artifact = push_one_artifact.clone();
+                    join_set.spawn(async move {
+                        pool.call_with_item(|stream| {
+                            push_one_artifact(
+                                stream,
+                                path,
+                                digest,
+                                success_callback,
+                            )
+                        }).await
+                    });
+                }
+            }
+        }
+        Ok(())
+    });
+}
--- crates/maelstrom-client-process/src/artifact_pusher/tcp_upload.rs
+++ crates/maelstrom-client-process/src/artifact_pusher/tcp_upload.rs
@@ -1,19 +1,13 @@
-use crate::artifact_pusher::{Message, Receiver, MAX_CLIENT_UPLOADS};
+use crate::artifact_pusher::{start_task_inner, Receiver};
 use crate::progress::{ProgressTracker, UploadProgressReader};
 use anyhow::{anyhow, Context as _, Result};
 use maelstrom_base::{
     proto::{ArtifactPusherToBroker, BrokerToArtifactPusher, Hello},
     Sha256Digest,
 };
-use maelstrom_util::{
-    async_fs::Fs, config::common::BrokerAddr, net, net::AsRawFdExt as _, r#async::Pool,
-};
+use maelstrom_util::{async_fs::Fs, config::common::BrokerAddr, net, net::AsRawFdExt as _};
 use slog::Logger;
-use std::{
-    num::NonZeroU32,
-    path::{Path, PathBuf},
-    sync::Arc,
-};
+use std::path::{Path, PathBuf};
 use tokio::{
     io::{self, AsyncReadExt as _},
     net::TcpStream,
@@ -27,14 +21,14 @@ fn construct_upload_name(digest: &Sha256Digest, path: &Path) -> String {
     format!("{short_digest} {file_name}")
 }
 
-async fn push_one_artifact(
+pub async fn push_one_artifact(
     stream: Option<TcpStream>,
     upload_tracker: ProgressTracker,
     broker_addr: BrokerAddr,
     path: PathBuf,
     digest: Sha256Digest,
     success_callback: Box<dyn FnOnce() + Send + Sync>,
-    log: &Logger,
+    log: Logger,
 ) -> Result<(TcpStream, ())> {
     push_one_artifact_inner(
         stream,
@@ -56,7 +50,7 @@ async fn push_one_artifact_inner(
     path: &Path,
     digest: Sha256Digest,
     success_callback: Box<dyn FnOnce() + Send + Sync>,
-    log: &Logger,
+    log: Logger,
 ) -> Result<(TcpStream, ())> {
     let mut stream = match stream {
         Some(stream) => stream,
@@ -64,7 +58,7 @@ async fn push_one_artifact_inner(
             let mut stream = TcpStream::connect(broker_addr.inner())
                 .await?
                 .set_socket_options()?;
-            net::write_message_to_async_socket(&mut stream, Hello::ArtifactPusher, log).await?;
+            net::write_message_to_async_socket(&mut stream, Hello::ArtifactPusher, &log).await?;
             stream
         }
     };
@@ -78,13 +72,13 @@ async fn push_one_artifact_inner(
 
     let mut file = UploadProgressReader::new(prog, file.chain(io::repeat(0)).take(size));
 
-    net::write_message_to_async_socket(&mut stream, ArtifactPusherToBroker(digest, size), log)
+    net::write_message_to_async_socket(&mut stream, ArtifactPusherToBroker(digest, size), &log)
         .await?;
     let copied = io::copy(&mut file, &mut stream).await?;
     assert_eq!(copied, size);
 
     let BrokerToArtifactPusher(resp) =
-        net::read_message_from_async_socket(&mut stream, log).await?;
+        net::read_message_from_async_socket(&mut stream, &log).await?;
 
     resp.map_err(|e| anyhow!("Error from broker: {e}"))?;
 
@@ -95,49 +89,24 @@ async fn push_one_artifact_inner(
 
 pub fn start_task(
     join_set: &mut JoinSet<Result<()>>,
-    mut receiver: Receiver,
+    receiver: Receiver,
     broker_addr: BrokerAddr,
     upload_tracker: ProgressTracker,
     log: Logger,
 ) {
-    let pool = Arc::new(Pool::new(
-        NonZeroU32::try_from(u32::try_from(MAX_CLIENT_UPLOADS).unwrap()).unwrap(),
-    ));
-    join_set.spawn(async move {
-        // When this join_set gets destroyed, all outstanding artifact pusher tasks will be
-        // canceled. That will happen either when our sender is closed, or when our own task is
-        // canceled. In either case, it means the process is shutting down.
-        //
-        // We have to be careful not to let the join_set grow indefinitely. This is why we select!
-        // below. We always wait on the join_set's join_next. If it's an actual error, we propagate
-        // it, which will immediately cancel all the other outstanding tasks.
-        let mut join_set = JoinSet::new();
-        loop {
-            tokio::select! {
-                Some(res) = join_set.join_next() => {
-                    res.unwrap()?; // We don't expect JoinErrors.
-                },
-                res = receiver.recv() => {
-                    let Some(Message { path, digest, success_callback } ) = res else { break; };
-                    let log = log.clone();
-                    let pool = pool.clone();
-                    let upload_tracker = upload_tracker.clone();
-                    join_set.spawn(async move {
-                        pool.call_with_item(|stream| {
-                            push_one_artifact(
-                                stream,
-                                upload_tracker,
-                                broker_addr,
-                                path,
-                                digest,
-                                success_callback,
-                                &log
-                            )
-                        }).await
-                    });
-                }
-            }
-        }
-        Ok(())
-    });
+    start_task_inner(
+        join_set,
+        receiver,
+        move |stream, path, digest, success_callback| {
+            push_one_artifact(
+                stream,
+                upload_tracker.clone(),
+                broker_addr,
+                path,
+                digest,
+                success_callback,
+                log.clone(),
+            )
+        },
+    )
 }
