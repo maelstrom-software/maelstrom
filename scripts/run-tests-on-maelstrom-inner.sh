6d4c29a9494eae72c642d5141e76f79bdfdbd4b8
Remi Bernotavicius <remi@abort.cc>
2024-12-31 22:40:27 -0800

    Change kill signal argument to be a number
    Apparently whatever version is on GitHub doesn't support how I had it
    before.

--- scripts/run-tests-on-maelstrom-inner.sh
+++ scripts/run-tests-on-maelstrom-inner.sh
@@ -41,7 +41,7 @@ CARGO_MAELSTROM_STATUS=$?
 cargo run --release --bin maelstrom-pytest -- --broker=localhost:$PORT $PYTEST_ARGS
 MAELSTROM_PYTEST_STATUS=$?
 
-kill --signal SIGTERM $BROKER_PID
+kill -15 $BROKER_PID
 rm "$TEMPFILE"
 set -e
 
