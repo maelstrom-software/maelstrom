6b26fc8120fb51120ee15ac4fd892462fd816bd7
Neal Fachan <neal@fachan.com>
2025-02-17 22:58:14 -0800

    Rewrite Watcher to not use scoped threads.
    We don't really need to create a thread in the first place.
    
    The main reason for this change is that I want the Watcher to be able to
    out-live the Client, and that won't work with scoped threads.
    
    There is another change here which is that the Watcher no longer pauses waiting
    for changes. Instead, that has to be added externally.

--- crates/maelstrom-test-runner/src/app/watch.rs
+++ crates/maelstrom-test-runner/src/app/watch.rs
@@ -1,99 +1,60 @@
 use anyhow::Result;
-use maelstrom_client::ProjectDir;
-use maelstrom_util::{root::Root, sync::Event as SyncEvent};
+use maelstrom_util::sync::Event as SyncEvent;
 use notify::{
     event::{Event as NotifyEvent, EventKind},
     RecommendedWatcher, RecursiveMode, Watcher as _,
 };
-use slog::{debug, Logger};
-use std::{collections::BTreeSet, path::PathBuf, sync::mpsc, thread::Scope, time::Duration};
+use std::{
+    path::{Path, PathBuf},
+    sync::Arc,
+};
 
-fn process_watch_events(
-    events: impl IntoIterator<Item = NotifyEvent>,
-    exclude_prefixes: &[PathBuf],
-) -> BTreeSet<PathBuf> {
-    events
-        .into_iter()
-        .filter_map(|event| {
-            if matches!(event.kind, EventKind::Access(_)) {
-                None
-            } else {
-                Some(event.paths)
-            }
-        })
-        .flatten()
-        .filter(|path| {
+pub struct Watcher {
+    _watcher: RecommendedWatcher,
+    done: Arc<SyncEvent>,
+}
+
+fn event_matches(event: &NotifyEvent, exclude_prefixes: &[PathBuf]) -> bool {
+    !matches!(event.kind, EventKind::Access(_))
+        && event.paths.iter().any(|path| {
             !exclude_prefixes
                 .iter()
                 .any(|exclude| path.starts_with(exclude))
         })
-        .collect()
 }
 
-pub struct Watcher<'deps, 'scope> {
-    scope: &'scope Scope<'scope, 'deps>,
-    log: Logger,
-    project_dir: &'deps Root<ProjectDir>,
-    watch_exclude_paths: &'deps Vec<PathBuf>,
-    done: &'deps SyncEvent,
-    files_changed: &'deps SyncEvent,
-}
-
-impl<'deps, 'scope> Watcher<'deps, 'scope> {
-    pub fn new(
-        scope: &'scope Scope<'scope, 'deps>,
-        log: Logger,
-        project_dir: &'deps Root<ProjectDir>,
-        watch_exclude_paths: &'deps Vec<PathBuf>,
-        done: &'deps SyncEvent,
-        files_changed: &'deps SyncEvent,
-    ) -> Self {
-        Self {
-            scope,
-            log,
-            project_dir,
-            watch_exclude_paths,
-            done,
-            files_changed,
-        }
-    }
-
-    pub fn watch_for_changes(&self) -> Result<()> {
-        let project_dir = self.project_dir;
-        let done = self.done;
-        let files_changed = self.files_changed;
-        let watch_exclude_paths = self.watch_exclude_paths;
-        let log = self.log.clone();
-
-        let (event_tx, event_rx) = mpsc::channel();
-        let mut watcher = RecommendedWatcher::new(event_tx, Default::default())?;
-        watcher.watch(project_dir.as_ref(), RecursiveMode::Recursive)?;
-
-        self.scope.spawn(move || {
-            // This loop attempts to batch up the events which happen around the same time. This
-            // is acting as a kind of debounce so we don't kick off two back-to-back test
-            // invocations every time we get a flurry of changes.
-            while done.wait_timeout(Duration::from_millis(100)).timed_out() {
-                let mut events = vec![];
-                while let Ok(event_res) = event_rx.try_recv() {
-                    if let Ok(event) = event_res {
-                        events.push(event);
-                    }
-                }
-                let changed_paths = process_watch_events(events, watch_exclude_paths);
-                if !changed_paths.is_empty() {
-                    debug!(log, "reacting to file changes"; "paths" => ?changed_paths);
-                    files_changed.set();
-                }
+impl Watcher {
+    pub fn new<I, T>(base: impl AsRef<Path>, exclude_prefixes: I) -> Result<Self>
+    where
+        I: IntoIterator<Item = T>,
+        T: AsRef<Path>,
+    {
+        let done = Arc::new(SyncEvent::new());
+        let done_clone = done.clone();
+        let exclude_prefixes = Vec::from_iter(
+            exclude_prefixes
+                .into_iter()
+                .map(|path| path.as_ref().to_owned()),
+        );
+        let handler = move |event| match event {
+            Err(_) => {
+                done_clone.set();
             }
-
-            drop(watcher);
-        });
-        Ok(())
+            Ok(event) if event_matches(&event, &exclude_prefixes) => {
+                done_clone.set();
+            }
+            Ok(_) => {}
+        };
+        let mut watcher = notify::recommended_watcher(handler)?;
+        watcher.watch(base.as_ref(), RecursiveMode::Recursive)?;
+        Ok(Self {
+            _watcher: watcher,
+            done,
+        })
     }
 
-    pub fn wait_for_changes(&self) {
-        self.files_changed.wait_and_unset();
+    pub fn wait(self) {
+        self.done.wait();
     }
 }
 
@@ -103,158 +64,123 @@ mod tests {
     use notify::event::{AccessKind, CreateKind, EventKind, ModifyKind, RemoveKind};
 
     #[track_caller]
-    fn process_watch_events_test<'a>(
-        events: impl IntoIterator<Item = NotifyEvent>,
-        watch_exclude_paths: impl IntoIterator<Item = &'a str>,
-        expected: impl IntoIterator<Item = &'a str>,
+    fn event_matches_test<'a>(
+        event: NotifyEvent,
+        exclude_paths: impl IntoIterator<Item = &'a str>,
+        expected: bool,
     ) {
-        let watch_exclude_paths = watch_exclude_paths
+        let exclude_paths = exclude_paths
             .into_iter()
             .map(Into::into)
             .collect::<Vec<_>>();
-        let changed_paths = process_watch_events(events, &watch_exclude_paths);
-        let expected_paths = expected.into_iter().map(Into::into).collect();
-        assert_eq!(changed_paths, expected_paths);
+        let actual = event_matches(&event, &exclude_paths);
+        assert_eq!(actual, expected);
     }
 
     #[test]
     fn files_any_processed() {
-        process_watch_events_test(
-            [
-                NotifyEvent::new(EventKind::Any)
-                    .add_path("foo.rs".into())
-                    .add_path("bar.rs".into()),
-                NotifyEvent::new(EventKind::Any).add_path("foo.rs".into()),
-                NotifyEvent::new(EventKind::Any).add_path("baz.rs".into()),
-                NotifyEvent::new(EventKind::Any).add_path("frob.rs".into()),
-                NotifyEvent::new(EventKind::Any),
-            ],
+        event_matches_test(
+            NotifyEvent::new(EventKind::Any)
+                .add_path("foo.rs".into())
+                .add_path("bar.rs".into()),
             [],
-            ["foo.rs", "bar.rs", "baz.rs", "frob.rs"],
-        )
+            true,
+        );
+        event_matches_test(NotifyEvent::new(EventKind::Any), [], false);
     }
 
     #[test]
     fn files_access_ignored() {
-        process_watch_events_test(
-            [
-                NotifyEvent::new(EventKind::Access(AccessKind::Any))
-                    .add_path("foo.rs".into())
-                    .add_path("bar.rs".into()),
-                NotifyEvent::new(EventKind::Access(AccessKind::Any)).add_path("foo.rs".into()),
-                NotifyEvent::new(EventKind::Access(AccessKind::Any)).add_path("baz.rs".into()),
-                NotifyEvent::new(EventKind::Access(AccessKind::Any)).add_path("frob.rs".into()),
-                NotifyEvent::new(EventKind::Access(AccessKind::Any)),
-            ],
+        event_matches_test(
+            NotifyEvent::new(EventKind::Access(AccessKind::Any))
+                .add_path("foo.rs".into())
+                .add_path("bar.rs".into()),
             [],
+            false,
+        );
+        event_matches_test(
+            NotifyEvent::new(EventKind::Access(AccessKind::Any)),
             [],
-        )
+            false,
+        );
     }
 
     #[test]
     fn files_created_processed() {
-        process_watch_events_test(
-            [
-                NotifyEvent::new(EventKind::Create(CreateKind::Any))
-                    .add_path("foo.rs".into())
-                    .add_path("bar.rs".into()),
-                NotifyEvent::new(EventKind::Create(CreateKind::Any)).add_path("foo.rs".into()),
-                NotifyEvent::new(EventKind::Create(CreateKind::Any)).add_path("baz.rs".into()),
-                NotifyEvent::new(EventKind::Create(CreateKind::Any)).add_path("frob.rs".into()),
-                NotifyEvent::new(EventKind::Create(CreateKind::Any)),
-            ],
+        event_matches_test(
+            NotifyEvent::new(EventKind::Create(CreateKind::Any))
+                .add_path("foo.rs".into())
+                .add_path("bar.rs".into()),
             [],
-            ["foo.rs", "bar.rs", "baz.rs", "frob.rs"],
-        )
+            true,
+        );
+        event_matches_test(
+            NotifyEvent::new(EventKind::Create(CreateKind::Any)),
+            [],
+            false,
+        );
     }
 
     #[test]
     fn files_modified_processed() {
-        process_watch_events_test(
-            [
-                NotifyEvent::new(EventKind::Modify(ModifyKind::Any))
-                    .add_path("foo.rs".into())
-                    .add_path("bar.rs".into()),
-                NotifyEvent::new(EventKind::Modify(ModifyKind::Any)).add_path("foo.rs".into()),
-                NotifyEvent::new(EventKind::Modify(ModifyKind::Any)).add_path("baz.rs".into()),
-                NotifyEvent::new(EventKind::Modify(ModifyKind::Any)).add_path("frob.rs".into()),
-                NotifyEvent::new(EventKind::Modify(ModifyKind::Any)),
-            ],
+        event_matches_test(
+            NotifyEvent::new(EventKind::Modify(ModifyKind::Any))
+                .add_path("foo.rs".into())
+                .add_path("bar.rs".into()),
             [],
-            ["foo.rs", "bar.rs", "baz.rs", "frob.rs"],
-        )
+            true,
+        );
+        event_matches_test(
+            NotifyEvent::new(EventKind::Modify(ModifyKind::Any)),
+            [],
+            false,
+        );
     }
 
     #[test]
     fn files_removed_processed() {
-        process_watch_events_test(
-            [
-                NotifyEvent::new(EventKind::Remove(RemoveKind::Any))
-                    .add_path("foo.rs".into())
-                    .add_path("bar.rs".into()),
-                NotifyEvent::new(EventKind::Remove(RemoveKind::Any)).add_path("foo.rs".into()),
-                NotifyEvent::new(EventKind::Remove(RemoveKind::Any)).add_path("baz.rs".into()),
-                NotifyEvent::new(EventKind::Remove(RemoveKind::Any)).add_path("frob.rs".into()),
-                NotifyEvent::new(EventKind::Remove(RemoveKind::Any)),
-            ],
+        event_matches_test(
+            NotifyEvent::new(EventKind::Remove(RemoveKind::Any))
+                .add_path("foo.rs".into())
+                .add_path("bar.rs".into()),
             [],
-            ["foo.rs", "bar.rs", "baz.rs", "frob.rs"],
-        )
+            true,
+        );
+        event_matches_test(
+            NotifyEvent::new(EventKind::Remove(RemoveKind::Any)),
+            [],
+            false,
+        );
     }
 
     #[test]
     fn files_other_processed() {
-        process_watch_events_test(
-            [
-                NotifyEvent::new(EventKind::Other)
-                    .add_path("foo.rs".into())
-                    .add_path("bar.rs".into()),
-                NotifyEvent::new(EventKind::Other).add_path("foo.rs".into()),
-                NotifyEvent::new(EventKind::Other).add_path("baz.rs".into()),
-                NotifyEvent::new(EventKind::Other).add_path("frob.rs".into()),
-                NotifyEvent::new(EventKind::Other),
-            ],
+        event_matches_test(
+            NotifyEvent::new(EventKind::Other)
+                .add_path("foo.rs".into())
+                .add_path("bar.rs".into()),
             [],
-            ["foo.rs", "bar.rs", "baz.rs", "frob.rs"],
-        )
+            true,
+        );
+        event_matches_test(NotifyEvent::new(EventKind::Other), [], false);
     }
 
     #[test]
     fn files_filtered_by_exclude_directories() {
-        process_watch_events_test(
-            [
-                NotifyEvent::new(EventKind::Any)
-                    .add_path("target/foo.rs".into())
-                    .add_path("target/bar.rs".into()),
-                NotifyEvent::new(EventKind::Any).add_path("target/foo.rs".into()),
-                NotifyEvent::new(EventKind::Any).add_path("target/baz.rs".into()),
-                NotifyEvent::new(EventKind::Any).add_path("target/frob.rs".into()),
-                NotifyEvent::new(EventKind::Any),
-            ],
-            ["target"],
-            [],
-        )
-    }
-
-    #[test]
-    fn kitchen_sink() {
-        process_watch_events_test(
-            [
-                NotifyEvent::new(EventKind::Any)
-                    .add_path("dir/subdir1/foo.rs".into())
-                    .add_path("dir/subdir2/bar.rs".into())
-                    .add_path("dir/subdir3/baz.rs".into()),
-                NotifyEvent::new(EventKind::Access(AccessKind::Any))
-                    .add_path("dir/subdir1/frob.rs".into())
-                    .add_path("dir/subdir2/fitz.rs".into())
-                    .add_path("dir/subdir3/quux.rs".into()),
-                NotifyEvent::new(EventKind::Modify(ModifyKind::Any))
-                    .add_path("dir/subdir3/quid.rs".into()),
-                NotifyEvent::new(EventKind::Modify(ModifyKind::Any))
-                    .add_path("dir/subdir2/bar.rs".into()),
-            ],
-            ["dir/subdir1", "dir/subdir2"],
-            ["dir/subdir3/baz.rs", "dir/subdir3/quid.rs"],
-        )
+        event_matches_test(
+            NotifyEvent::new(EventKind::Any)
+                .add_path("exclude1/foo.rs".into())
+                .add_path("exclude2/bar.rs".into()),
+            ["exclude1", "exclude2"],
+            false,
+        );
+        event_matches_test(
+            NotifyEvent::new(EventKind::Any)
+                .add_path("exclude1/foo.rs".into())
+                .add_path("exclude2/bar.rs".into())
+                .add_path("/bar.rs".into()),
+            ["exclude1", "exclude2"],
+            true,
+        );
     }
 }
