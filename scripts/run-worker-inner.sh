ebc29886565f6b06620cd48eaa6e0bf6b127c9d9
Neal Fachan <neal@fachan.com>
2025-02-21 23:24:20 -0800

    Up the log level for workers in CI.

--- scripts/run-worker-inner.sh
+++ scripts/run-worker-inner.sh
@@ -6,4 +6,4 @@ export ACTIONS_RUNTIME_TOKEN=$1
 export ACTIONS_RESULTS_URL=$2
 WORKER_ARGS="--artifact-transfer-strategy github --broker-connection github"
 
-cargo run --release --bin maelstrom-worker -- --broker=0.0.0.0:0 $WORKER_ARGS || true
+cargo run --release --bin maelstrom-worker -- --log-level=debug --broker=0.0.0.0:0 $WORKER_ARGS || true
