d4955d6e743662b9eab0441db9655e51af492e97
Neal Fachan <neal@fachan.com>
2026-09-24 12:53:14 -0700

    Lint test code too.
    Run clippy with --all-targets. Test code had never been linted; the Rust
    1.98.1 update cleaned it up.

--- scripts/lint-inner.sh
+++ scripts/lint-inner.sh
@@ -3,7 +3,7 @@
 set -ex
 
 cargo-fmt --check
-cargo-clippy clippy -- -Dwarnings
+cargo-clippy clippy --all-targets -- -Dwarnings
 cargo xtask publish --lint
 cargo check --all-targets
 #(cd doc/book && mdbook test)
